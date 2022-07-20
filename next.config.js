6ac14451ffd2ead399e37c4ad876e7293013871f
gitul0515 <gitul0515@gmail.com>
2022-07-20 21:11:33 +0900

    chore: 절대 경로 설정

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  webpack(config) {
+    config.resolve.modules.push(__dirname);
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
