dccb13418eac9718a193c338e094e2f37ce547cc
권기홍 <gitul0515@gmail.com>
2022-07-29 14:27:57 +0900

    design: Layout 컴포넌트 추가 (#23)
    * design: html의 font-size를 62.5%로 변경
    
    * design: Layout 컴포넌트 추가
    
    * design: PageWrapper 컴포넌트로 페이지의 width를 가변 조절
    
    * design: media query의 breakPoint 상수화
    
    * rename: theme을 constants 폴더로 이동
    
    * rename: index.css를 global.css로 이름 수정
    
    * refactor: fontface 파일로 font 관련 코드 분리
    
    * design: _app에 Layout 컴포넌트 추가

---
+++ components/Layout/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <h1>푸터</h1>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ components/Layout/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>헤더</h1>
+    </header>
+  );
+};
+
+export default Header;
