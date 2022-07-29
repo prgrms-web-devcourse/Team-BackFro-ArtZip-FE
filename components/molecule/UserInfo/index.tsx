65889ed2e5c372091a85046f5255bdfd9a0fb5d2
Daeun Kim <o_o___t@pusan.ac.kr>
2022-07-30 05:40:14 +0900

    feat: userInfo 컴포넌트 구현

---
+++ components/molecule/UserInfo/index.tsx
@@ -0,0 +1,54 @@
+import { Avatar } from 'antd';
+import styled from '@emotion/styled';
+import Link from 'next/link';
+
+interface UserInfoProps {
+  image: string;
+  userName: string;
+  createdDate: Date;
+  userId: number;
+}
+
+const UserInfo = ({ image, userName, createdDate, userId }: UserInfoProps) => {
+  return (
+    <UserInfoContainer>
+      <Link href={`/user/${userId}`}>
+        <UserInfoAvatar src={image} size={48} />
+      </Link>
+      <UserInfoTextWrapper>
+        <Link href={`/user/${userId}`}>
+          <UserInfoName>{userName}</UserInfoName>
+        </Link>
+        <UserInfoDate>{createdDate.toLocaleDateString()}</UserInfoDate>
+      </UserInfoTextWrapper>
+    </UserInfoContainer>
+  );
+};
+
+const UserInfoAvatar = styled(Avatar)`
+  &:hover {
+    cursor: pointer;
+    filter: brightness(70%);
+  }
+`;
+
+const UserInfoContainer = styled.div`
+  display: flex;
+  width: fit-content;
+`;
+
+const UserInfoTextWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const UserInfoName = styled.span`
+  &:hover {
+    cursor: pointer;
+    text-decoration: underline;
+  }
+`;
+
+const UserInfoDate = styled.span``;
+
+export default UserInfo;
