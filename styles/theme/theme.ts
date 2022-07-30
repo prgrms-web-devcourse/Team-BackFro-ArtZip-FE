a2248116a27951ef9e6cced2df654aa9c6b0bfe0
gitul0515 <gitul0515@gmail.com>
2022-07-30 15:42:50 +0900

    refactor: breakPoint를 theme에 추가

--- styles/theme/theme.ts
+++ styles/theme/theme.ts
@@ -1,14 +1,15 @@
 import { Theme } from '@emotion/react';
 
 const color = {
+  red: '#FC3B41',
   blue: {
     main: '#242F9B',
     dark: '#646FD4',
     light: '#9BA3EB',
   },
-  red: '#FC3B41',
+  black: '#000000',
   font: {
-    black: '#3C3C3C',
+    main: '#3C3C3C',
     dark: '#828282',
     light: '#A3A3A3',
   },
@@ -17,10 +18,17 @@ const color = {
     light: '#F1F1F1',
   },
   background: '#F8F8F8',
+  white: '#FFFFFF',
+};
+
+const breakPoint = {
+  tablet: '1023px',
+  mobile: '767px',
 };
 
 const theme: Theme = {
   color,
+  breakPoint,
 };
 
 export default theme;
