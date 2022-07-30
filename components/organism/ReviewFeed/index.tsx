8908548699b2acf3f25b157723ce148eafe7477b
Daeun Kim <74234333+dar-jeeling@users.noreply.github.com>
2022-07-31 00:25:04 +0900

    feature: ReviewFeed 컴포넌트 개발 (#33)
    * feat: likeToggle 컴포넌트 구현
    
    * feat: LikeInfo 컴포넌트 구현
    
    * feat: CommentInfo 컴포넌트 구현
    
    * feat: userInfo 컴포넌트 구현
    
    * feat: molecule 의 컴포넌트 모듈화
    
    * feat: ReviewFeed 컴포넌트 개발
    
    * design: 댓글 아이콘 변경
    
    * feat: 날짜 보여주기 함수 구현
    
    * fix: 날짜 표현 방식 변경
    
    * refactor: 변수 이름 목적에 맞게 변경
    
    * refactor: SEO를 위하여 Link 밑에 a 태그 추가

---
+++ components/organism/ReviewFeed/index.tsx
@@ -0,0 +1,167 @@
+import { UserInfo, LikeInfo, CommentInfo } from 'components/molecule';
+import styled from '@emotion/styled';
+import { Card, Button } from 'antd';
+import Link from 'next/link';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+
+interface ReviewFeedProps {
+  userProfileImage: string;
+  userName: string;
+  userId: number;
+  feedCreateDate: Date;
+  exhibitionName: string;
+  exhibitionId: number;
+  feedTitle: string;
+  feedContent: string;
+  isLiked: boolean;
+  likeCount: number;
+  onLikeClick: () => void;
+  commentCount: number;
+  isMyFeed: boolean;
+  reviewId: number;
+  onDeleteButtonClick: () => void;
+  reviewThumbnailImage: string;
+}
+
+const ReviewFeed = ({
+  userProfileImage,
+  userName,
+  userId,
+  feedCreateDate,
+  exhibitionName,
+  exhibitionId,
+  feedTitle,
+  feedContent,
+  isLiked,
+  likeCount,
+  onLikeClick,
+  commentCount,
+  isMyFeed,
+  reviewId,
+  onDeleteButtonClick,
+  reviewThumbnailImage,
+}: ReviewFeedProps) => {
+  const router = useRouter();
+
+  return (
+    <Card>
+      <ReviewFeedWrapper>
+        <ReviewFeedContent>
+          <ReviewFeedHeader>
+            <UserInfo
+              image={userProfileImage}
+              userName={userName}
+              createdDate={feedCreateDate}
+              userId={userId}
+            ></UserInfo>
+            <ReviewDetailLink href={`/exhibitions/detail/${exhibitionId}`}>
+              <a># {exhibitionName}</a>
+            </ReviewDetailLink>
+          </ReviewFeedHeader>
+
+          <ReviewFeedMain
+            onClick={() => {
+              router.push(`/reviews/detail/${reviewId}`);
+            }}
+          >
+            <ReviewTitle>{feedTitle}</ReviewTitle>
+            <ReviewContent>{feedContent}</ReviewContent>
+          </ReviewFeedMain>
+
+          <ReviewFeedBottom>
+            <InfoGroup>
+              <LikeInfo isLiked={isLiked} likeCount={likeCount} onClick={onLikeClick} />
+              <CommentInfo commentCount={commentCount} />
+            </InfoGroup>
+
+            {isMyFeed && (
+              <FeedButtonGroup>
+                <Button type="text">
+                  <Link href={`/reviews/${reviewId}/edit`}>
+                    <a>수정</a>
+                  </Link>
+                </Button>
+                <Button type="text" onClick={onDeleteButtonClick}>
+                  삭제
+                </Button>
+              </FeedButtonGroup>
+            )}
+          </ReviewFeedBottom>
+        </ReviewFeedContent>
+
+        <ReviewFeedThumbnail
+          onClick={() => {
+            router.push(`/reviews/detail/${reviewId}`);
+          }}
+        >
+          <Image src={reviewThumbnailImage} width="200" height="200" alt="Review Thumbnail" />
+        </ReviewFeedThumbnail>
+      </ReviewFeedWrapper>
+    </Card>
+  );
+};
+
+const ReviewFeedWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const ReviewFeedContent = styled.div`
+  flex-grow: 1;
+  margin-right: 10px;
+`;
+
+const ReviewFeedThumbnail = styled.div`
+  &:hover {
+    filter: brightness(80%);
+  }
+`;
+
+const ReviewFeedHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const ReviewDetailLink = styled(Link)`
+  // TODO: 테마 색 지정
+  &:hover {
+    // TODO: 호버 시 강조색 지정
+  }
+`;
+
+const ReviewFeedMain = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const ReviewTitle = styled.h2``;
+
+const ReviewContent = styled.p``;
+
+const ReviewFeedBottom = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+`;
+
+const InfoGroup = styled.div`
+  display: flex;
+  gap: 5px;
+  align-content: center;
+`;
+
+const FeedButtonGroup = styled.div`
+  display: flex;
+  align-content: center;
+`;
+
+export default ReviewFeed;
---
+++ utils/index.ts
@@ -0,0 +1,1 @@
+export { default as displayDate } from './displayDate';
