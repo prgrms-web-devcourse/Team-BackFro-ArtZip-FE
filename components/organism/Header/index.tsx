4345fdf3a6b26701cb7f3fd74e69472a05f27cd3
gitul0515 <gitul0515@gmail.com>
2022-07-30 20:12:08 +0900

    design: header 마크업 수정 및 css 추가

--- components/organism/Header/index.tsx
+++ components/organism/Header/index.tsx
@@ -2,6 +2,7 @@ import styled from '@emotion/styled';
 import Logo from 'components/atoms/Logo';
 import { Input } from 'antd';
 import Link from 'next/link';
+import { css } from '@emotion/css';
 
 const { Search } = Input;
 
@@ -9,17 +10,22 @@ const Header = () => {
   return (
     <StyledHeader>
       <Container>
-        <Logo />
-        <Search
-          placeholder="전시회를 검색해주세요."
-          allowClear
-          style={{
-            width: 200,
-          }}
-        />
+        <Link href="/">
+          <a>
+            <Logo />
+          </a>
+        </Link>
+        <Utility>
+          <Link href="/signin" passHref>
+            <StyledA>로그인</StyledA>
+          </Link>
+          <Link href="/signup" passHref>
+            <StyledA>회원가입</StyledA>
+          </Link>
+        </Utility>
       </Container>
-      <Navigation>
-        <PageLinks>
+      <Container>
+        <Navigation>
           <Link href="/search-result" passHref>
             <StyledA>맞춤 전시회</StyledA>
           </Link>
@@ -29,47 +35,82 @@ const Header = () => {
           <Link href="/community" passHref>
             <StyledA>커뮤니티</StyledA>
           </Link>
-        </PageLinks>
-        <UserLinks>
-          <Link href="/signin" passHref>
-            <StyledA>로그인</StyledA>
-          </Link>
-          <Link href="/signup" passHref>
-            <StyledA>회원가입</StyledA>
-          </Link>
-        </UserLinks>
-      </Navigation>
+        </Navigation>
+        <Search
+          placeholder="전시회를 검색해주세요."
+          allowClear
+          style={{
+            width: 200,
+          }}
+        />
+      </Container>
     </StyledHeader>
   );
 };
 
+const dynamicWidth = () => css`
+  width: 80%;
+  max-width: 1400px;
+  margin: 0 auto;
+
+  @media (max-width: 767px) {
+    width: 90%;
+  }
+`;
+
 const StyledHeader = styled.header`
-  width: 100%;
-  height: 200px;
   position: fixed;
   top: 0;
   left: 0;
+  width: 100%;
+  padding-top: 10px;
   border-bottom: 1px solid #dddddd;
+  background-color: #fafafa;
 `;
 
 const Container = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  width: 80%;
+  max-width: 1400px;
+  margin: 0 auto;
+
+  &:first-of-type {
+    margin-bottom: 10px;
+  }
+
+  @media (max-width: 767px) {
+    width: 90%;
+  }
+`;
+
+const Utility = styled.div`
+  & > a:first-of-type {
+    margin-right: 20px;
+  }
 `;
 
 const Navigation = styled.nav`
   display: flex;
   justify-content: space-between;
   align-items: center;
-`;
 
-const PageLinks = styled.div`
-  display: flex;
-`;
+  & > a {
+    margin-right: 80px;
+    padding-bottom: 20px;
+    border-bottom: 4px solid transparent;
+    margin-bottom: -3px;
 
-const UserLinks = styled.div`
-  display: flex;
+    &:hover {
+      font-weight: 500;
+      border-bottom: 4px solid #242f9b;
+    }
+  }
+
+  & > a:last-of-type {
+    margin-right: 0;
+  }
 `;
 
 const StyledA = styled.a`
