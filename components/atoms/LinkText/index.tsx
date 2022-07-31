1c09ee078cbd98429a1ef2b062ebc33728be22da
권기홍 <gitul0515@gmail.com>
2022-08-01 02:39:08 +0900

    feat: Header 컴포넌트 추가  (#39)
    * feat: Logo 컴포넌트 추가
    
    * design: header 마크업 추가
    
    * design: header 마크업 수정 및 css 추가
    
    * design: SearchBar 커스텀 스타일링 추가
    
    * feat: LinkText 컴포넌트 추가
    
    * feat: Logo 컴포넌트 추가
    
    * refactor: header에 LinkText, Logo 컴포넌트 적용
    
    * rerfactor: PageWrapper의 태그를 div로 변경
    
    * design: 전역 글자색, 배경색 적용
    
    * design: 모바일 font-size 변경
    
    * feat: 현재 페이지인 경우 LinkText의 스타일 강조

---
+++ components/atoms/LinkText/index.tsx
@@ -0,0 +1,36 @@
+import Link from 'next/link';
+import styled from '@emotion/styled';
+import { CSSProperties } from 'react';
+
+interface LinkTextProps {
+  href: string;
+  text: string;
+  textStyle?: CSSProperties;
+  isCurrentPage?: boolean;
+}
+
+const LinkText = ({ href, text, textStyle, isCurrentPage }: LinkTextProps) => {
+  return (
+    <Link href={href} passHref>
+      <StyledA style={textStyle} isCurrentPage={isCurrentPage}>
+        {text}
+      </StyledA>
+    </Link>
+  );
+};
+
+const StyledA = styled.a<{
+  isCurrentPage?: boolean;
+}>`
+  font-size: 2.6rem;
+
+  ${({ isCurrentPage }) =>
+    isCurrentPage &&
+    ` 
+    color: #242f9b;
+    font-weight: 500;
+    border-bottom: 4px solid #242f9b;
+  `}
+`;
+
+export default LinkText;
--- components/organism/Header/index.tsx
+++ components/organism/Header/index.tsx
@@ -1,20 +1,151 @@
 import styled from '@emotion/styled';
+import Logo from 'components/atoms/Logo';
+import { Input } from 'antd';
+import LinkText from 'components/atoms/LinkText';
+import { useRouter } from 'next/router';
 
 const Header = () => {
+  const { pathname } = useRouter();
+
   return (
     <StyledHeader>
-      <h1>헤더</h1>
+      <Container>
+        <Logo width={252} height={92} />
+        <Utility>
+          <LinkText href="/signin" text="로그인" />
+          <LinkText href="/signup" text="회원가입" />
+        </Utility>
+      </Container>
+      <Container>
+        <Navigation>
+          <LinkText
+            href="/search-result"
+            text="맞춤 전시회"
+            isCurrentPage={pathname === '/search-result'}
+          />
+          <LinkText
+            href="/reviews/create"
+            text="감상평 쓰기"
+            isCurrentPage={pathname === '/reviews/create'}
+          />
+          <LinkText href="/community" text="커뮤니티" isCurrentPage={pathname === '/community'} />
+        </Navigation>
+        <SearchBar placeholder="전시회를 검색해주세요." allowClear />
+      </Container>
     </StyledHeader>
   );
 };
 
 const StyledHeader = styled.header`
-  width: 100%;
-  height: 200px;
   position: fixed;
   top: 0;
   left: 0;
-  background-color: ${({ theme }) => theme.color.blue.main};
+  width: 100%;
+  padding-top: 10px;
+  border-bottom: 1px solid #dddddd;
+  background-color: #fafafa;
+`;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 80%;
+  max-width: 1400px;
+  margin: 0 auto;
+
+  &:first-of-type {
+    margin-bottom: 10px;
+  }
+
+  @media (max-width: 767px) {
+    width: 90%;
+  }
+`;
+
+const Utility = styled.div`
+  & > a:first-of-type {
+    margin-right: 20px;
+  }
+`;
+
+const Navigation = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  & > a {
+    margin-right: 80px;
+    padding-bottom: 20px;
+    margin-bottom: -3px;
+
+    &:last-of-type {
+      margin-right: 0;
+    }
+
+    &:hover {
+      font-weight: 500;
+      border-bottom: 4px solid #242f9b;
+    }
+
+    @media (max-width: 767px) {
+      margin-right: auto;
+    }
+  }
+
+  @media (max-width: 767px) {
+    width: 100%;
+  }
+`;
+
+const SearchBar = styled(Input.Search)`
+  // TODO: 스타일 초기화 - 재사용
+  * {
+    border: 0;
+    outline: none;
+    background: none;
+
+    &:hover,
+    &:active,
+    &:focus,
+    &:focus-within {
+      border: 0;
+      outline: none;
+      box-shadow: none;
+      background: none;
+      --antd-wave-shadow-color: none;
+    }
+  }
+
+  width: 400px;
+  margin-bottom: 20px;
+  border: 2px solid #242f9b;
+  border-radius: 18px;
+  overflow: hidden;
+
+  .ant-input-affix-wrapper {
+    padding: 8px 16px;
+  }
+
+  .ant-input {
+    font-size: 1.8rem;
+  }
+
+  .ant-input-clear-icon {
+    font-size: 1.8rem;
+  }
+
+  .ant-input-search-button {
+    margin-right: 10px;
+  }
+
+  .anticon-search {
+    font-size: 2.4rem;
+  }
+
+  @media (max-width: 767px) {
+    display: none;
+  }
 `;
 
 export default Header;
