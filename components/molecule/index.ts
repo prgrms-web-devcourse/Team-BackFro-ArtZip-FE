8dc0fdfe3d2cc17a94b707cde40f146f69135acf
gitul0515 <gitul0515@gmail.com>
2022-08-01 02:05:30 +0900

    refactor: Banner를 import하는 방식 수정

--- components/molecule/index.ts
+++ components/molecule/index.ts
@@ -1,3 +1,4 @@
 export { default as UserInfo } from './UserInfo';
 export { default as LikeInfo } from './LikeInfo';
 export { default as CommentInfo } from './CommentInfo';
+export { default as Banner } from './Banner';
