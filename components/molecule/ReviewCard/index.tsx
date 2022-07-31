6066f624dcb23d6947c36f2d4e6eea1960aaa4a3
yunjjeongjo <two6885@gmail.com>
2022-08-01 01:16:26 +0900

    feat: ReviewCard 구현

---
+++ components/molecule/ReviewCard/index.tsx
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+import { HeartOutlined, MessageOutlined } from '@ant-design/icons';
+import * as S from './style';
+import Link from 'next/link';
+import { displayDate } from 'utils';
+
+interface ReviewCardProps {
+  reviewId: number;
+  thumbnail: string;
+  title: string;
+  content: string;
+  createdAt: string;
+  userId: number;
+  profileImage: string;
+  nickname: string;
+  likeCount: number;
+  commentCount: number;
+  photo: string | null;
+}
+
+const ReviewCard = ({
+  reviewId,
+  thumbnail,
+  title,
+  content,
+  createdAt,
+  userId,
+  profileImage,
+  nickname,
+  likeCount,
+  commentCount,
+  photo,
+}: ReviewCardProps) => {
+  const [isHover, setIsHover] = useState(false);
+  const mouseHover = () => setIsHover((isHover) => !isHover);
+  return (
+    <>
+      <Link href={`reviews/detail/${reviewId}`}>
+        <S.ReviewCard>
+          <S.PhotoWrapper onMouseEnter={mouseHover} onMouseLeave={mouseHover}>
+            <S.Photo
+              src={'https://www.culture.go.kr/upload/rdf/22/07/show_2022071816261910020.jpg'}
+            ></S.Photo>
+            {isHover ? (
+              <S.HoverContent>
+                <HeartOutlined /> {likeCount} <MessageOutlined /> {commentCount}
+              </S.HoverContent>
+            ) : null}
+          </S.PhotoWrapper>
+          <S.UserInfoContainer>
+            <Link href={`/user/${userId}`}>
+              <S.UserInfoAvatar src={'https://joeschmoe.io/api/v1/random'} size={60} />
+            </Link>
+            <S.UserInfoTextWrapper>
+              <Link href={`/user/${userId}`}>
+                <S.UserInfoName>{nickname}</S.UserInfoName>
+              </Link>
+              <S.UserInfoDate>{displayDate(new Date(createdAt))}</S.UserInfoDate>
+            </S.UserInfoTextWrapper>
+          </S.UserInfoContainer>
+          <S.Title>{title}</S.Title>
+          <S.Content>{content}</S.Content>
+        </S.ReviewCard>
+      </Link>
+    </>
+  );
+};
+
+export default ReviewCard;
---
+++ components/molecule/ReviewCard/style.ts
@@ -0,0 +1,95 @@
+import styled from '@emotion/styled';
+import { Avatar } from 'antd';
+import { Card } from 'antd';
+
+export const ReviewCard = styled(Card)`
+  width: 600px;
+  height: 270px;
+  border: none;
+  border-radius: 30px;
+  padding: 10px 5px 10px 5px;
+`;
+
+export const Title = styled.div`
+  font-size: 25px;
+  font-weight: 700;
+  margin-bottom: 5px;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+export const Content = styled.div`
+  width: 500px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  font-weight: 400;
+  font-size: 18px;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+export const UserInfoAvatar = styled(Avatar)`
+  margin-right: 10px;
+  &:hover {
+    cursor: pointer;
+    filter: brightness(70%);
+  }
+`;
+
+export const UserInfoContainer = styled.div`
+  display: flex;
+  width: fit-content;
+  margin-bottom: 15px;
+`;
+
+export const UserInfoTextWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const UserInfoName = styled.span`
+  font-size: 22px;
+  font-weight: 700;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+export const UserInfoDate = styled.span`
+  font-weight: 300;
+  color: #a3a3a3;
+`;
+
+export const PhotoWrapper = styled.div``;
+
+export const Photo = styled.img`
+  width: 110px;
+  height: 110px;
+  border-radius: 20px;
+  position: absolute;
+  top: 25px;
+  right: 25px;
+`;
+
+export const HoverContent = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 110px;
+  height: 110px;
+  border-radius: 20px;
+  background-color: rgba(0, 0, 0, 0.3);
+  gap: 5px;
+  color: white;
+  position: absolute;
+  top: 25px;
+  right: 25px;
+  &:hover {
+    cursor: pointer;
+  }
+`;
