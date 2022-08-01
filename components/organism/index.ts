b9c6518ab251a3301fdf8e0bd486d5a02bc0aeab
gitul0515 <gitul0515@gmail.com>
2022-08-02 03:46:44 +0900

    refactor: ImageUpload의 import 방식 수정

--- components/organism/index.ts
+++ components/organism/index.ts
@@ -1,3 +1,4 @@
 export { default as Header } from './Header';
 export { default as Footer } from './Footer';
 export { default as ReviewFeed } from './ReviewFeed';
+export { default as ImageUpload } from './ImageUpload';
