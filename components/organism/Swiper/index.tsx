93761246fc9d479a6a9e996bfa3f4ca1c9080014
Hyevvy <hyeg0@naver.com>
2022-08-01 17:58:04 +0900

    feat: Home 마크업

--- components/organism/Swiper/index.tsx
+++ components/organism/Swiper/index.tsx
@@ -14,7 +14,6 @@ const SwiperWrapper = ({items} : SwiperProps) => {
         <S.SwiperWrapper>
             <div className="parent">
                     <Swiper className="swiper-container"
-                        modules={[Autoplay]}
                         spaceBetween={20}
                         slidesPerView={3}
                         autoplay={{
