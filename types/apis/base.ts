349da2538d5f1ca49d2316d9b92bea0e35081a25
Hyevvy <hyeg0@naver.com>
2022-08-01 16:54:03 +0900

    feat: api interface 정의

--- types/apis/base.ts
+++ types/apis/base.ts
@@ -1,4 +1,5 @@
 export interface BaseProps{
     message?: string;
     status?: number;
+    code?: string;
 }
--- types/apis/user/index.ts
+++ types/apis/user/index.ts
@@ -2,17 +2,23 @@ import { ExhibitionCardProps, ReviewProps, UserProps } from "types/model";
 import { BaseProps } from "../base";
 
 export interface UserRegisterProps {
-    userId: number;
-    nickname: string;
-    email: string;
+    data:{
+        userId: number;
+        nickname: string;
+        email: string;
+    }
 }
 
 export interface UserInfoReviewLikeProps{
-    content: ReviewProps[];
+    data: {
+        content: ReviewProps[];
+    }
 }
 
 export interface UserInfoExhibitionLikeProps{
-    content: ExhibitionCardProps[];
+    data:{
+        content: ExhibitionCardProps[];
+    }
 }
 
 // export interface UserMePasswordProps{
@@ -24,17 +30,21 @@ export interface UserInfoExhibitionLikeProps{
 // }
 
 export interface UserInfoProps extends BaseProps{
-    userId: number;
-    profileImage: string;
-    email: string;
-    reviewCount: number;
-    likeCount: number;
-    commentCount: number;
-    reviews: ReviewProps[];
+    data:{
+        userId: number;
+        profileImage: string;
+        email: string;
+        reviewCount: number;
+        likeCount: number;
+        commentCount: number;
+        reviews: ReviewProps[];
+    }
 }
 
 export interface UserTokenProps {
-    accessToken: string;
+    data:{
+        accessToken: string;
+    }
 }
 
 export type UserOauthLoginProps = UserProps
