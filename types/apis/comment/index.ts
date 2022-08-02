6d4eca535c2ac1714afc7b5cacc182e6076dc996
Hyevvy <72402747+Hyevvy@users.noreply.github.com>
2022-08-02 15:06:43 +0900

    feat: api interface 정의 (#56)
    * feat: User api interface 정의
    
    * feat: base interface 정의
    
    * feat: api interface 정의
    
    * fix: optional 추가
    
    * fix: ExhibitionCardProps 를 ExhibitionProps로 수정
    
    * fix: response 이름 수정
    
    * fix: response 이름 수정
    
    * fix: response 이름 수정
    
    * fix: response 이름 수정
    
    * fix: Props를 Response로 수정

---
+++ types/apis/comment/index.ts
@@ -0,0 +1,52 @@
+import { CommentProps } from 'antd';
+import { UserProps } from 'types/model';
+import { BaseResponse } from '../base';
+
+//댓글 조회
+export interface CommentReadResponse extends BaseResponse {
+  data?: {
+    content: CommentProps[];
+    numberOfElements: number;
+    offset: number;
+    pageNumber: number;
+    pageSize: number;
+    totalElements: number;
+    totalPages: number;
+  };
+}
+
+//댓글 삭제
+export interface CommentDeleteExhibitionResponse extends BaseResponse {
+  data?: {
+    commentId: number;
+    createdAt: string;
+    isDeleted: boolean;
+  };
+}
+
+//댓글 수정
+export interface CommentEditExhibitionResponse extends BaseResponse {
+  data?: {
+    commentId: number;
+    content: string;
+    createdAt: string;
+    updatedAt: string;
+    isEdited: boolean;
+    isDeleted: boolean;
+    user: UserProps;
+  };
+}
+
+//댓글 생성
+
+export interface CommentCreateExhibitionProps extends BaseResponse {
+  data?: {
+    commentId: number;
+    content: string;
+    createdAt: string;
+    updatedAt: string;
+    isEdited: boolean;
+    isDeleted: boolean;
+    user: UserProps[];
+  };
+}
