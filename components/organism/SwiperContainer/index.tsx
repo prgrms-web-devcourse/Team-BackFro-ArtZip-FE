4dca2c07e0944938059fe3d3759da3a397afefde
Hyevvy <72402747+Hyevvy@users.noreply.github.com>
2022-08-02 14:37:09 +0900

    feat: 메인페이지 마크업 (#57)
    * fix: SwiperContainer를 SwiperWrapper로 수정
    
    * feat: SwiperContainer 구현
    
    * feat: Home 마크업
    
    * feat: Home 마크업
    
    * test: 중간 커밋
    
    Co-authored-by: Daeun Kim <o_o___t@pusan.ac.kr>

---
+++ components/organism/SwiperContainer/index.tsx
@@ -0,0 +1,29 @@
+import React, { ReactElement } from 'react';
+import * as S from './style';
+import Link from 'next/link';
+
+interface SwiperContainerProps {
+  title: string;
+  children: ReactElement;
+  type: string;
+}
+
+const SwiperContainer = ({ title, children, type }: SwiperContainerProps) => {
+  return (
+    <S.SwiperContainer>
+      <div className="swiper-container-head">
+        <h3>{title}</h3>
+        <Link
+          href={{
+            pathname: '/search-result',
+            query: { type },
+          }}
+        >
+          <p>더보기</p>
+        </Link>
+      </div>
+      <div>{children}</div>
+    </S.SwiperContainer>
+  );
+};
+export default SwiperContainer;
