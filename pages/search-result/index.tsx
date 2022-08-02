b8c4a888bfb23f8bc07edaec1c706b37ee96ae8e
Hyevvy <hyeg0@naver.com>
2022-08-02 17:04:10 +0900

    test: 중간 커밋

--- pages/search-result/index.tsx
+++ pages/search-result/index.tsx
@@ -2,10 +2,12 @@ import Head from 'next/head';
 import { Pagination } from 'antd';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
+import * as S from './style';
 
 const SearchResultPage = () => {
   const router = useRouter();
   const { exhibition } = router.query;
+  const { type } = router.query;
 
   const [currentPage, setCurrentPage] = useState(1);
 
@@ -13,18 +15,33 @@ const SearchResultPage = () => {
   const [total, setTotal] = useState(100);
 
   return (
-    <>
+    <S.SearchResultContainer>
       <Head>
         <title>ArtZip | SearchResult</title>
       </Head>
-      <div>SearchResult: {exhibition}</div>
+      {type === 'upcoming' && (
+        <div>
+          <h2>다가오는 전시회</h2>
+        </div>
+      )}
+      {type === 'popular' && (
+        <div>
+          <h2>인기많은 전시회</h2>
+        </div>
+      )}
+      {exhibition && <div>SearchResult: {exhibition}</div>}
+
       <Pagination
+        className="pagination"
         defaultCurrent={1}
         current={currentPage}
         total={total}
+        defaultPageSize={10}
+        showSizeChanger={false}
+        pageSize={10}
         onChange={(page) => setCurrentPage(page)}
       />
-    </>
+    </S.SearchResultContainer>
   );
 };
 
---
+++ pages/search-result/style.ts
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+export const SearchResultContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  .pagination {
+    margin-top: 20px;
+  }
+`;
