ccc2b214f019d7db49544df4215d9774d6a9d7ef
Hyevvy <hyeg0@naver.com>
2022-08-02 16:55:01 +0900

    feat: pagination import

--- pages/search-result/index.tsx
+++ pages/search-result/index.tsx
@@ -1,9 +1,16 @@
 import Head from 'next/head';
+import { Pagination } from 'antd';
 import { useRouter } from 'next/router';
+import { useState } from 'react';
 
 const SearchResultPage = () => {
   const router = useRouter();
   const { exhibition } = router.query;
+
+  const [currentPage, setCurrentPage] = useState(1);
+
+  //TODO : 서버애서 받은 total 값 넣을 예정
+  const [total, setTotal] = useState(100);
 
   return (
     <>
@@ -11,6 +18,12 @@ const SearchResultPage = () => {
         <title>ArtZip | SearchResult</title>
       </Head>
       <div>SearchResult: {exhibition}</div>
+      <Pagination
+        defaultCurrent={1}
+        current={currentPage}
+        total={total}
+        onChange={(page) => setCurrentPage(page)}
+      />
     </>
   );
 };
