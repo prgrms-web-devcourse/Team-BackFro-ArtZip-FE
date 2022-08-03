07701e91a23597afbcb2c982b5a7dd5a17f80658
Hyevvy <hyeg0@naver.com>
2022-08-04 00:23:23 +0900

    feat: search toolbar 마크업

--- components/organism/SearchToolbar/index.tsx
+++ components/organism/SearchToolbar/index.tsx
@@ -1,19 +1,62 @@
 import * as S from './style';
-import { HomeOutlined } from '@ant-design/icons';
+import { HomeOutlined, CalendarOutlined } from '@ant-design/icons';
 
 interface SearchToolbarProps {
-  type: 'place' | 'time';
+  type: 'place' | 'period';
 }
+
+const placeArr = [
+  '모든 지역',
+  '서울',
+  '경기',
+  '인천',
+  '강원',
+  '부산',
+  '경남',
+  '대구',
+  '경북',
+  '대전',
+  '울산',
+  '해외',
+];
+
+const periodArr = [
+  '모든 날짜',
+  '1월',
+  '2월',
+  '3월',
+  '4월',
+  '5월',
+  '6월',
+  '7월',
+  '8월',
+  '9월',
+  '10월',
+  '11월',
+  '12월',
+];
 const SearchToolbar = ({ type }: SearchToolbarProps) => {
   return (
     <S.SearchToolbar>
-      {type === 'place' && (
+      {type === 'place' ? (
         <S.IconBox>
           <S.Icon>
             <HomeOutlined />
           </S.Icon>
         </S.IconBox>
+      ) : (
+        <S.IconBox>
+          <S.Icon>
+            <CalendarOutlined />
+          </S.Icon>
+        </S.IconBox>
       )}
+      <S.ContentBox>
+        <div className="content-box-title">{type === 'place' ? '개최장소' : '모든지역'}</div>
+        <div className="content-box-sub">
+          {type === 'place' && placeArr.map((it, idx) => <div key={idx}>{it}</div>)}
+        </div>
+      </S.ContentBox>
     </S.SearchToolbar>
   );
 };
--- components/organism/SearchToolbar/style.ts
+++ components/organism/SearchToolbar/style.ts
@@ -1,15 +1,36 @@
 import styled from '@emotion/styled';
 
-export const SearchToolbar = styled.div``;
+export const SearchToolbar = styled.div`
+  display: flex;
+  gap: 1rem;
+`;
 
 export const IconBox = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  flex-grow: 1;
   background-color: #ececec;
   width: 50px;
   border-radius: 50%;
 `;
 export const Icon = styled.span`
   font-size: 30px;
+`;
+
+export const ContentBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 3;
+  .content-box-title {
+    cursor: pointer;
+    font-weight: 700;
+    white-space: no-wrap;
+  }
+  .content-box-sub {
+    display: flex;
+    flex-wrap: nowrap;
+    gap: 1rem;
+    white-space: nowrap;
+  }
 `;
