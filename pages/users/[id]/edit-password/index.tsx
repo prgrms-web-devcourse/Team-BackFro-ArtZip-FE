1e4aac422a58e2925752619bd2e6018e3cf7cca8
gitul0515 <gitul0515@gmail.com>
2022-08-05 23:08:32 +0900

    feat: 유저 페이지를 하위 페이지 3개로 분리

---
+++ pages/users/[id]/edit-password/index.tsx
@@ -0,0 +1,5 @@
+const UserEditPasswordPage = () => {
+  return <div></div>;
+};
+
+export default UserEditPasswordPage;
---
+++ pages/users/[id]/edit/index.tsx
@@ -0,0 +1,5 @@
+const UserEditPage = () => {
+  return <div></div>;
+};
+
+export default UserEditPage;
