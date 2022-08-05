5e3e77bef1f39c528f82c1d0690eedc11abe3da1
gitul0515 <gitul0515@gmail.com>
2022-08-05 14:26:50 +0900

    FEAT: exhibitionAPI 추가

---
+++ apis/exhibition/index.ts
@@ -0,0 +1,22 @@
+import { unAuthRequest } from 'apis/common';
+
+const exhibitionAPI = {
+  getUpcoming: (page: number, size: number) => {
+    return unAuthRequest.get(`/api/v1/exhibitions/upcoming?page=${page}&size=${size}`);
+  },
+  getMostLike: (page: number, size: number, includeEnd: boolean) => {
+    return unAuthRequest.get(
+      `/api/v1/exhibitions/mostlike?page=${page}&size=${size}&include-end=${includeEnd}`,
+    );
+  },
+  getDetail: (exhibitionId: number) => {
+    return unAuthRequest.get(`/api/v1/exhibitions/${exhibitionId}`);
+  },
+  search: (query: string, page: number, size: number, includeEnd: boolean) => {
+    return unAuthRequest.get(
+      `/api/v1/exhibitions?query=${query}&page=${page}&size=${size}&include-end=${includeEnd}`,
+    );
+  },
+};
+
+export default exhibitionAPI;
