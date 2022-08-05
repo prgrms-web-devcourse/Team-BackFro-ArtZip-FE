c46d914d0fc56a55a567ab0127ec3fe444c32940
Daeun Kim <o_o___t@pusan.ac.kr>
2022-08-05 17:43:30 +0900

    test : comment API 모킹

--- mocks/handlers/comment/index.ts
+++ mocks/handlers/comment/index.ts
@@ -1,12 +1,11 @@
 import { rest } from 'msw';
 
 //  기준 데이터 : 43번 리뷰 (코멘트 갯수가 안맞을 지도)
-
 const COMMENTS = {
   data: {
     content: [
       {
-        commentId: 0,
+        commentId: 1,
         content: '꼭 가고 싶네요. 근데 시간이 될지ㅠㅠ',
         createdAt: '2022-07-26T11:26:24',
         updatedAt: '2022-07-26T11:28:49',
@@ -15,31 +14,70 @@ const COMMENTS = {
         user: {
           userId: 0,
           nickname: '미스터공공',
-          profileImage: 'https://i.pravatar.cc/300?1​',
+          profileImage: 'https://joeschmoe.io/api/v1/random',
         },
-        children: [
-          {
-            commentId: 1,
-            content: '같이 가용~',
-            createdAt: '2022-07-26T11:26:24',
-            updatedAt: null,
-            isEdited: false,
-            isDeleted: false,
-            user: {
-              userId: 1,
-              nickname: '그린',
-              profileImage: 'https://i.pravatar.cc/300?​2',
-            },
-          },
-        ],
+        childrenCount: 1,
       },
     ],
-    numberOfElements: 2, //content의 요소가 몇개인지
+    numberOfElements: 1, //content의 요소가 몇개인지
     offset: 0, // 현재 페이지에서 시작하는 요소의 index 번호
     pageNumber: 0, //페이지 넘버
     pageSize: 20, //페이지 사이즈
-    totalElements: 2, // 전체 요소 수
+    totalElements: 1, // 전체 요소 수
     totalPages: 1, //전체 페이지 수
+  },
+};
+
+// 기준데이터 : commentId가 0인 댓글에 대한 대댓글
+const REPLYS = {
+  data: {
+    content: [
+      {
+        commentId: 11,
+        content: '같이가요~~',
+        createdAt: '2022-07-26T11:26:24',
+        updatedAt: '2022-07-26T11:28:49',
+        isEdited: true,
+        isDeleted: false,
+        user: {
+          userId: 0,
+          nickname: '그린',
+          profileImage: 'https://joeschmoe.io/api/v1/random',
+        },
+      },
+      {
+        commentId: 12,
+        content: '같이가요~~',
+        createdAt: '2022-07-26T11:26:24',
+        updatedAt: '2022-07-26T11:28:49',
+        isEdited: false,
+        isDeleted: false,
+        user: {
+          userId: 0,
+          nickname: '그린',
+          profileImage: 'https://joeschmoe.io/api/v1/random',
+        },
+      },
+      {
+        commentId: 13,
+        content: '같이가요~~',
+        createdAt: '2022-07-26T11:26:24',
+        updatedAt: '2022-07-26T11:28:49',
+        isEdited: true,
+        isDeleted: false,
+        user: {
+          userId: 0,
+          nickname: '그린',
+          profileImage: 'https://joeschmoe.io/api/v1/random',
+        },
+      },
+    ],
+    numberOfElements: 3, //content의 요소가 몇개인지
+    offset: 0, // 현재 페이지에서 시작하는 요소의 index 번호
+    pageNumber: 0, //페이지 넘버
+    pageSize: 10, //페이지 사이즈
+    totalElements: 1, // 전체 요소 수
+    totalPages: 3, //전체 페이지 수
   },
 };
 
@@ -48,21 +86,22 @@ const CommentHandlers = [
   rest.get(
     `${process.env.MOCKING_API_END_POINT}api/v1/reviews/:reviewId/comments`,
     (req, res, ctx) => {
-      // 43번 기준이므로 무조건 43
+      // 43번 기준이므로 review는 무조건 43번 리뷰
       // const { reviewId } = req.params;
       const page = req.url.searchParams.get('page');
       const size = req.url.searchParams.get('size');
 
-      const single_review_success = {
-        message: '후기 단건 성공',
+      const comments_success = {
+        message: '댓글 조회 성공',
         code: 200,
         data: {
           ...COMMENTS,
         },
       };
-      return res(ctx.json(single_review_success));
+      return res(ctx.json(comments_success));
     },
   ),
+
   // 댓글 삭제
   rest.delete(`${process.env.MOCKING_API_END_POINT}api/v1/comments/:commentId`, (req, res, ctx) => {
     const { commentId } = req.params;
@@ -97,9 +136,10 @@ const CommentHandlers = [
       );
 
       COMMENTS.data.content[targetIndex].content = UPDATE_CONTENT;
+      COMMENTS.data.content[targetIndex].isEdited = true;
 
       const DELETE_COMMENT_SUCCESS = {
-        message: '댓글 삭제 성공',
+        message: '댓글 수정 성공',
         code: 200,
         data: {
           ...COMMENTS.data.content[targetIndex],
@@ -108,60 +148,99 @@ const CommentHandlers = [
       return res(ctx.json(DELETE_COMMENT_SUCCESS));
     },
   ),
-  // 리뷰에 댓글 생성
+  // 댓글 생성
+
   rest.post(
     `${process.env.MOCKING_API_END_POINT}api/v1/reviews/:reviewId/comments`,
     async (req, res, ctx) => {
       const commentId = COMMENTS.data.content.length + 3;
       const { content, parentId } = await req.json();
 
-      // parentId가 없는 경우
+      // parentId가 있는 경우 (대댓글)
       if (parentId) {
-        const parentIndex = COMMENTS.data.content.findIndex(
-          (comment) => comment.commentId.toString() === parentId,
-        );
-
-        COMMENTS.data.content[parentIndex].children.push({
+        // 대댓글 생성 로직
+        REPLYS.data.content.push({
           commentId: commentId,
           content: content,
           createdAt: '2022-07-26T11:26:24',
-          updatedAt: null,
+          updatedAt: '',
           isEdited: false,
           isDeleted: false,
           user: {
-            userId: 1,
-            nickname: '그린',
-            profileImage: 'https://i.pravatar.cc/300?​2',
-          },
-        });
-      } else {
-        COMMENTS.data.content.push({
-          commentId: 0,
-          content: content,
-          createdAt: '2022-07-26T11:26:24',
-          updatedAt: '2022-07-26T11:28:49',
-          isEdited: true,
-          isDeleted: false,
-          user: {
             userId: 0,
-            nickname: '미스터공공',
-            profileImage: 'https://i.pravatar.cc/300?1',
+            nickname: '대댓글',
+            profileImage: 'https://joeschmoe.io/api/v1/random?42',
           },
-          children: [],
         });
+
+        COMMENTS.data.content[parentId].childrenCount += 1;
+
+        return res(
+          ctx.delay(100),
+          ctx.status(200),
+          ctx.json({
+            message: '댓글 생성 성공',
+            code: 201,
+            data: {
+              commentId: commentId,
+              content: content,
+              createdAt: '2022-07-26T11:26:24',
+              updatedAt: '',
+              isEdited: false,
+              isDeleted: false,
+              user: {
+                userId: 0,
+                nickname: '대댓글',
+                profileImage: 'https://joeschmoe.io/api/v1/random?42',
+              },
+            },
+          }),
+        );
       }
-      // parentId가 있는 경우
+      // 일반 댓글의 경우
+      const newComment = {
+        commentId: commentId,
+        content: content,
+        createdAt: '2022-07-26T11:26:24',
+        updatedAt: '',
+        isEdited: false,
+        isDeleted: false,
+        user: {
+          userId: 0,
+          nickname: '대래대래댓댓댓글',
+          profileImage: 'https://joeschmoe.io/api/v1/random?23',
+        },
+        childrenCount: 0,
+      };
+
+      COMMENTS.data.content.push(newComment);
+
       return res(
         ctx.delay(100),
         ctx.status(200),
         ctx.json({
           message: '댓글 생성 성공',
           code: 201,
-          data: {
-            reviewId: Math.floor(Math.random() * 101),
-          },
+          data: newComment,
         }),
       );
+    },
+  ),
+
+  // 대댓글 조회 (TODO: 미구현, 우선은 댓글을 그대로 리턴하도록)
+  rest.get(
+    `${process.env.MOCKING_API_END_POINT}api/v1/comments/:commentId/children`,
+    (req, res, ctx) => {
+      const { commentId } = req.params;
+
+      const reply_success = {
+        message: '대댓글 조회 성공',
+        code: 200,
+        data: {
+          ...REPLYS.data.content,
+        },
+      };
+      return res(ctx.json(reply_success));
     },
   ),
 ];
