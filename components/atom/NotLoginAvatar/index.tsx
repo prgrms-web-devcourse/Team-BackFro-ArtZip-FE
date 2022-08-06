6a2dd91fabbd201bd20a93f06588db7b850b9a27
Daeun Kim <o_o___t@pusan.ac.kr>
2022-08-07 01:05:39 +0900

    feat: 로그인 하지 않은 유저 아바타 컴포넌트 구현

---
+++ components/atom/NotLoginAvatar/index.tsx
@@ -0,0 +1,23 @@
+import { UserOutlined } from '@ant-design/icons';
+import styled from '@emotion/styled';
+import { Avatar } from 'antd';
+import Link from 'next/link';
+
+const NotLoginAvatar = ({ size = 48 }) => {
+  return (
+    <Link href="/signin">
+      <StyledAvatar size={size} icon={<UserOutlined />} />
+    </Link>
+  );
+};
+
+const StyledAvatar = styled(Avatar)`
+  cursor: pointer;
+  margin-right: 10px;
+
+  &:hover {
+    filter: brightness(70%);
+  }
+`;
+
+export default NotLoginAvatar;
