be1937ae46b06e6e1326e995338d71ad896cdc4f
gitul0515 <gitul0515@gmail.com>
2022-08-07 16:12:23 +0900

    design: 사용자 정보 페이지 마크업

--- pages/users/[id]/index.tsx
+++ pages/users/[id]/index.tsx
@@ -1,23 +1,11 @@
 import styled from '@emotion/styled';
-import { Tabs, Image, Input, Upload, Form, Button } from 'antd';
+import { Tabs, Image, Button } from 'antd';
 import Link from 'next/link';
+import { ReviewCard, ExhibitionCard } from 'components/molecule';
 
 const UserPage = () => {
   return (
     <PageContainer>
-      <NavigationButtons>
-        <Link href="/users/1/edit">
-          <a>
-            <NavigationButton type="primary">유저 정보</NavigationButton>
-          </a>
-        </Link>
-        <Link href="/users/1/edit-password">
-          <a>
-            <NavigationButton type="default">계정 관리</NavigationButton>
-          </a>
-        </Link>
-      </NavigationButtons>
-
       <ProfileContainer>
         <ProfileImage
           src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
@@ -29,31 +17,262 @@ const UserPage = () => {
       </ProfileContainer>
       <TabCardContainer type="card" tabPosition="top" centered>
         <Tab tab="작성한 글 (12)" key={1}>
-          작성한 글
+          <ReviewContainer>
+            <ReviewCard
+              reviewId={reviewDummy.reviewId}
+              thumbnail={reviewDummy.exhibition.thumbnail}
+              title={reviewDummy.title}
+              content={reviewDummy.content}
+              createdAt={reviewDummy.createdAt}
+              likeCount={reviewDummy.likeCount}
+              commentCount={reviewDummy.commentCount}
+              photo={reviewDummy.photos[0]}
+              userId={reviewDummy.user.userId}
+              nickname={reviewDummy.user.nickname}
+              profileImage={reviewDummy.user.profileImage}
+            />
+            <ReviewCard
+              reviewId={reviewDummy.reviewId}
+              thumbnail={reviewDummy.exhibition.thumbnail}
+              title={reviewDummy.title}
+              content={reviewDummy.content}
+              createdAt={reviewDummy.createdAt}
+              likeCount={reviewDummy.likeCount}
+              commentCount={reviewDummy.commentCount}
+              photo={reviewDummy.photos[0]}
+              userId={reviewDummy.user.userId}
+              nickname={reviewDummy.user.nickname}
+              profileImage={reviewDummy.user.profileImage}
+            />
+            <ReviewCard
+              reviewId={reviewDummy.reviewId}
+              thumbnail={reviewDummy.exhibition.thumbnail}
+              title={reviewDummy.title}
+              content="오늘 번아웃증후군 전시회를 다녀왔다. 정말 재밌었다~~ 오늘 번아웃증후군 전시회를 다녀왔다. 정말 재밌었다~~ 오늘 번아웃증후군 전시회를 다녀왔다. 정말 재밌었다~~ 오늘 번아웃증후군 전시회를 다녀왔다. 정말 재밌었다~~ 오늘 번아웃증후군 전시회를 다녀왔다. 정말 재밌었다~~ 오늘 번아웃증후군 전시회를 다녀왔다. 정말 재밌었다~~"
+              createdAt={reviewDummy.createdAt}
+              likeCount={reviewDummy.likeCount}
+              commentCount={reviewDummy.commentCount}
+              photo={reviewDummy.photos[0]}
+              userId={reviewDummy.user.userId}
+              nickname={reviewDummy.user.nickname}
+              profileImage={reviewDummy.user.profileImage}
+            />
+            <ReviewCard
+              reviewId={reviewDummy.reviewId}
+              thumbnail={reviewDummy.exhibition.thumbnail}
+              title={reviewDummy.title}
+              content="오늘 번아웃증후군 전시회를 다녀왔다. 정말 재밌었다~~ "
+              createdAt={reviewDummy.createdAt}
+              likeCount={reviewDummy.likeCount}
+              commentCount={reviewDummy.commentCount}
+              photo={reviewDummy.photos[0]}
+              userId={reviewDummy.user.userId}
+              nickname={reviewDummy.user.nickname}
+              profileImage={reviewDummy.user.profileImage}
+            />
+            <ReviewCard
+              reviewId={reviewDummy.reviewId}
+              thumbnail={reviewDummy.exhibition.thumbnail}
+              title={reviewDummy.title}
+              content="오늘 번아웃증후군 전시회를 다녀왔다. 정말 재밌었다~~ 오늘 번아웃증후군 전시회를 다녀왔다. 정말 재밌었다~~ 오늘 번아웃증후군 전시회를 다녀왔다. 정말 재밌었다~~ 오늘 번아웃증후군 전시회를 다녀왔다. 정말 재밌었다~~ 오늘 번아웃증후군 전시회를 다녀왔다. 정말 재밌었다~~ 오늘 번아웃증후군 전시회를 다녀왔다. 정말 재밌었다~~"
+              createdAt={reviewDummy.createdAt}
+              likeCount={reviewDummy.likeCount}
+              commentCount={reviewDummy.commentCount}
+              photo={reviewDummy.photos[0]}
+              userId={reviewDummy.user.userId}
+              nickname={reviewDummy.user.nickname}
+              profileImage={reviewDummy.user.profileImage}
+            />
+          </ReviewContainer>
         </Tab>
         <Tab tab="좋아하는 전시회 (6)" key={2}>
-          좋아하는 전시회
+          <ExhibitionContainer>
+            <ExhibitionCard
+              exhibitionId={exhibitionDummy.exhibitionId}
+              name={exhibitionDummy.name}
+              thumbnail={exhibitionDummy.thumbnail}
+              startDate={exhibitionDummy.startDate}
+              endDate={exhibitionDummy.endDate}
+              likeCount={exhibitionDummy.likeCount}
+              reviewCount={exhibitionDummy.reviewCount}
+              isLiked={exhibitionDummy.isLiked}
+            />
+            <ExhibitionCard
+              exhibitionId={exhibitionDummy.exhibitionId}
+              name={exhibitionDummy.name}
+              thumbnail={exhibitionDummy.thumbnail}
+              startDate={exhibitionDummy.startDate}
+              endDate={exhibitionDummy.endDate}
+              likeCount={exhibitionDummy.likeCount}
+              reviewCount={exhibitionDummy.reviewCount}
+              isLiked={exhibitionDummy.isLiked}
+            />
+            <ExhibitionCard
+              exhibitionId={exhibitionDummy.exhibitionId}
+              name={exhibitionDummy.name}
+              thumbnail={exhibitionDummy.thumbnail}
+              startDate={exhibitionDummy.startDate}
+              endDate={exhibitionDummy.endDate}
+              likeCount={exhibitionDummy.likeCount}
+              reviewCount={exhibitionDummy.reviewCount}
+              isLiked={exhibitionDummy.isLiked}
+            />
+            <ExhibitionCard
+              exhibitionId={exhibitionDummy.exhibitionId}
+              name={exhibitionDummy.name}
+              thumbnail={exhibitionDummy.thumbnail}
+              startDate={exhibitionDummy.startDate}
+              endDate={exhibitionDummy.endDate}
+              likeCount={exhibitionDummy.likeCount}
+              reviewCount={exhibitionDummy.reviewCount}
+              isLiked={exhibitionDummy.isLiked}
+            />
+            <ExhibitionCard
+              exhibitionId={exhibitionDummy.exhibitionId}
+              name={exhibitionDummy.name}
+              thumbnail={exhibitionDummy.thumbnail}
+              startDate={exhibitionDummy.startDate}
+              endDate={exhibitionDummy.endDate}
+              likeCount={exhibitionDummy.likeCount}
+              reviewCount={exhibitionDummy.reviewCount}
+              isLiked={exhibitionDummy.isLiked}
+            />
+            <ExhibitionCard
+              exhibitionId={exhibitionDummy.exhibitionId}
+              name={exhibitionDummy.name}
+              thumbnail={exhibitionDummy.thumbnail}
+              startDate={exhibitionDummy.startDate}
+              endDate={exhibitionDummy.endDate}
+              likeCount={exhibitionDummy.likeCount}
+              reviewCount={exhibitionDummy.reviewCount}
+              isLiked={exhibitionDummy.isLiked}
+            />
+            <ExhibitionCard
+              exhibitionId={exhibitionDummy.exhibitionId}
+              name={exhibitionDummy.name}
+              thumbnail={exhibitionDummy.thumbnail}
+              startDate={exhibitionDummy.startDate}
+              endDate={exhibitionDummy.endDate}
+              likeCount={exhibitionDummy.likeCount}
+              reviewCount={exhibitionDummy.reviewCount}
+              isLiked={exhibitionDummy.isLiked}
+            />
+          </ExhibitionContainer>
         </Tab>
         <Tab tab="좋아하는 후기 (17)" key={3}>
-          좋아하는 후기
+          <ReviewContainer>
+            <ReviewCard
+              reviewId={reviewDummy.reviewId}
+              thumbnail={reviewDummy.exhibition.thumbnail}
+              title={reviewDummy.title}
+              content={reviewDummy.content}
+              createdAt={reviewDummy.createdAt}
+              likeCount={reviewDummy.likeCount}
+              commentCount={reviewDummy.commentCount}
+              photo={reviewDummy.photos[0]}
+              userId={reviewDummy.user.userId}
+              nickname={reviewDummy.user.nickname}
+              profileImage={reviewDummy.user.profileImage}
+            />
+            <ReviewCard
+              reviewId={reviewDummy.reviewId}
+              thumbnail={reviewDummy.exhibition.thumbnail}
+              title={reviewDummy.title}
+              content={reviewDummy.content}
+              createdAt={reviewDummy.createdAt}
+              likeCount={reviewDummy.likeCount}
+              commentCount={reviewDummy.commentCount}
+              photo={reviewDummy.photos[0]}
+              userId={reviewDummy.user.userId}
+              nickname={reviewDummy.user.nickname}
+              profileImage={reviewDummy.user.profileImage}
+            />
+            <ReviewCard
+              reviewId={reviewDummy.reviewId}
+              thumbnail={reviewDummy.exhibition.thumbnail}
+              title={reviewDummy.title}
+              content={reviewDummy.content}
+              createdAt={reviewDummy.createdAt}
+              likeCount={reviewDummy.likeCount}
+              commentCount={reviewDummy.commentCount}
+              photo={reviewDummy.photos[0]}
+              userId={reviewDummy.user.userId}
+              nickname={reviewDummy.user.nickname}
+              profileImage={reviewDummy.user.profileImage}
+            />
+            <ReviewCard
+              reviewId={reviewDummy.reviewId}
+              thumbnail={reviewDummy.exhibition.thumbnail}
+              title={reviewDummy.title}
+              content={reviewDummy.content}
+              createdAt={reviewDummy.createdAt}
+              likeCount={reviewDummy.likeCount}
+              commentCount={reviewDummy.commentCount}
+              photo={reviewDummy.photos[0]}
+              userId={reviewDummy.user.userId}
+              nickname={reviewDummy.user.nickname}
+              profileImage={reviewDummy.user.profileImage}
+            />
+            <ReviewCard
+              reviewId={reviewDummy.reviewId}
+              thumbnail={reviewDummy.exhibition.thumbnail}
+              title={reviewDummy.title}
+              content={reviewDummy.content}
+              createdAt={reviewDummy.createdAt}
+              likeCount={reviewDummy.likeCount}
+              commentCount={reviewDummy.commentCount}
+              photo={reviewDummy.photos[0]}
+              userId={reviewDummy.user.userId}
+              nickname={reviewDummy.user.nickname}
+              profileImage={reviewDummy.user.profileImage}
+            />
+          </ReviewContainer>
         </Tab>
       </TabCardContainer>
+
+      <NavigationButtons>
+        <Link href="/users/1">
+          <a>
+            <NavigationButton type="primary" size="large">
+              사용자 정보
+            </NavigationButton>
+          </a>
+        </Link>
+        <Link href="/users/1/edit">
+          <a>
+            <NavigationButton type="default" size="large">
+              프로필 수정
+            </NavigationButton>
+          </a>
+        </Link>
+        <Link href="/users/1/edit-password">
+          <a>
+            <NavigationButton type="default" size="large">
+              비밀번호 변경
+            </NavigationButton>
+          </a>
+        </Link>
+      </NavigationButtons>
     </PageContainer>
   );
 };
 
+const NavigationButtons = styled.nav`
+  position: absolute;
+  top: 20px;
+  left: 0;
+`;
+
+const NavigationButton = styled(Button)`
+  display: block;
+  width: 130px;
+  margin-bottom: 2px;
+`;
+
 const PageContainer = styled.div`
   position: relative;
-  max-width: 1000px;
+  max-width: 1100px;
   margin: 0 auto;
-  text-align: center;
-`;
-
-const TabContainer = styled(Tabs)`
-  margin-top: 50px;
-`;
-
-const Tab = styled(Tabs.TabPane)`
   text-align: center;
 `;
 
@@ -83,25 +302,79 @@ const TabCardContainer = styled(Tabs)`
   margin-top: 30px;
 `;
 
-const NavigationButtons = styled.nav`
-  position: absolute;
-  top: 20px;
-  left: 0;
+const Tab = styled(Tabs.TabPane)`
+  text-align: left;
 `;
 
-const NavigationButton = styled(Button)`
-  display: block;
-  margin-bottom: 2px;
-  padding-right: 24px;
-  padding-left: 24px;
+const ReviewContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  justify-content: center;
+  justify-items: center;
+  gap: 10px;
+
+  /* flex-wrap: wrap;
+  justify-content: center; */
+
+  padding-left: 70px;
+  padding-right: 70px;
+  padding-bottom: 30px;
+
+  @media screen and (max-width: ${({ theme }) => theme.breakPoint.mobile}) {
+    grid-template-columns: 1fr;
+  }
 `;
 
-const ProfileEditForm = styled.form``;
+const ExhibitionContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  justify-items: center;
+  padding-bottom: 30px;
 
-const PasswordEditForm = styled.form``;
+  @media screen and (max-width: ${({ theme }) => theme.breakPoint.tablet}) {
+    grid-template-columns: repeat(2, 1fr);
+  }
 
-const FieldSet = styled.fieldset``;
+  @media screen and (max-width: ${({ theme }) => theme.breakPoint.mobile}) {
+    grid-template-columns: 1fr;
+  }
+`;
 
-const Legend = styled.legend``;
+const reviewDummy = {
+  reviewId: 43,
+  user: {
+    userId: 11,
+    profileImage: 'https~',
+    nickname: 'Emily',
+  },
+  exhibition: {
+    exhibitionId: 24,
+    name: '전시회 이름',
+    startDate: '2022-10-11',
+    thumbnail: 'https~~',
+  },
+  title: '번아웃증후군 전시회 다녀옴~',
+  content:
+    '오늘 핸드아트코리아 전시회를 다녀왔다. 정말 재밌었다~~ 오늘 핸드아트코리아 전시회를 다녀왔다. 정말 재밌었다~~',
+  createdAt: '2022-03-22T22:33:11',
+  updatedAt: '2022-03-23T13:03:51',
+  isEdited: true,
+  isLiked: false,
+  isPublic: true,
+  likeCount: 32,
+  commentCount: 2,
+  photos: ['https~', 'https~'],
+};
+
+const exhibitionDummy = {
+  exhibitionId: 1,
+  name: '번아웃증후군',
+  thumbnail: 'https://www.culture.go.kr/upload/rdf/22/07/show_2022071816261910020.jpg',
+  startDate: '2022-08-04',
+  endDate: '2022-08-10',
+  isLiked: false,
+  likeCount: 5,
+  reviewCount: 3,
+};
 
 export default UserPage;
