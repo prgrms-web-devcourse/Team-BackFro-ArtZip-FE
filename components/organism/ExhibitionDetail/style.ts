e8263382a5ec316d666d11ed9b4a685485abf781
yunjjeongjo <93373357+yunjjeongjo@users.noreply.github.com>
2022-08-07 22:09:07 +0900

    feat: 전시회 상세 페이지 리뷰 반영 (#112)
    * design: 불필요한 flex 속성 제거
    
    * feat: self-closing tag 사용
    
    * design: 태그 변경
    
    * feat: useRouter 사용
    
    * fix: 전시회 상세 페이지 style파일 이름 수정
    
    * feat: ExhibitionInfo 컴포넌트 분리
    
    * feat: ExhibitionDetail 컴포넌트 분리
    
    * design: Description background 색상 변경
    
    * design: 불필요한 코드 삭제
    
    * feat: 구조 분해 할당 사용
    
    * design: CSS 수정
    
    * feat: 컴포넌트 적용
    
    * design: Button 간격 수정

---
+++ components/organism/ExhibitionDetail/style.ts
@@ -0,0 +1,70 @@
+import styled from '@emotion/styled';
+import { Image } from 'antd';
+
+export const ExhibitionContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 75%;
+  @media screen and (max-width: ${({ theme }) => theme.breakPoint.tablet}) {
+    width: 90%;
+  }
+  @media screen and (max-width: ${({ theme }) => theme.breakPoint.mobile}) {
+    width: 100%;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+export const Thumbnail = styled(Image)`
+  margin-top: 10px;
+  width: 320px;
+  height: 450px;
+  box-shadow: 0 14px 28px rgba(0, 0, 0, 0.05), 0 10px 10px rgba(0, 0, 0, 0.05);
+  @media screen and (max-width: ${({ theme }) => theme.breakPoint.tablet}) {
+    width: 240px;
+    height: 350px;
+  }
+  @media screen and (max-width: ${({ theme }) => theme.breakPoint.mobile}) {
+    width: 300px;
+    height: 420px;
+    margin-bottom: 40px;
+    position: relative;
+  }
+`;
+
+export const InfoContainer = styled.div`
+  @media screen and (max-width: ${({ theme }) => theme.breakPoint.mobile}) {
+    margin-left: 20px;
+    width: 90%;
+  }
+`;
+
+export const Title = styled.h2`
+  width: 80%;
+  font-weight: 700;
+  font-size: 3rem;
+  padding-bottom: 10px;
+  margin-bottom: 20px;
+  border-bottom: 1px solid ${({ theme }) => theme.color.font.light};
+`;
+
+export const IconContainer = styled.div`
+  height: 7rem;
+  margin-top: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 3rem;
+  color: ${({ theme }) => theme.color.font.dark};
+  cursor: pointer;
+`;
+
+export const Container = styled.div`
+  margin-left: 40px;
+  width: 55%;
+  display: flex;
+  @media screen and (max-width: ${({ theme }) => theme.breakPoint.mobile}) {
+    width: 80%;
+  }
+`;
