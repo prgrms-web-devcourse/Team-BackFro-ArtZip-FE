2dbf5589ab23bec3742a8bffee75c1b060cf3085
gitul0515 <gitul0515@gmail.com>
2022-08-07 23:50:26 +0900

    refactor: Antd의 Form 컴포넌트로 변경

--- pages/reviews/create/index.tsx
+++ pages/reviews/create/index.tsx
@@ -1,70 +1,89 @@
-import Head from 'next/head';
 import styled from '@emotion/styled';
-import { Input, DatePicker, Switch, Image, Button } from 'antd';
+import {
+  Input,
+  DatePicker,
+  Switch,
+  Image,
+  Button,
+  message,
+  InputRef,
+  Form,
+  DatePickerProps,
+} from 'antd';
 import { Banner } from 'components/molecule';
 import { ImageUpload } from 'components/organism';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 const ReviewCreatePage = () => {
+  const searchBarRef = useRef<InputRef>(null);
   const [isPublic, setIsPublic] = useState(true);
 
-  const handleSearch = (value: string) => {
-    console.log(value);
+  const handleSearch = (searchWord: string) => {
+    if (!/\S/.test(searchWord)) {
+      message.warning('한 글자 이상 입력해주세요.');
+
+      if (searchBarRef.current) {
+        searchBarRef.current.input!.value = '';
+      }
+    }
+    console.log(searchWord);
   };
 
   const handleSwitchChange = (checked: boolean) => {
     setIsPublic(checked);
   };
 
+  const handleDateChange: DatePickerProps['onChange'] = (date, dateString) => {
+    console.log(date, dateString);
+  };
+
   return (
     <>
-      <Head>
-        <title>ArtZip | ReviewCreatePage</title>
-      </Head>
       <Section>
         <Banner
           title="후기 쓰기"
           content="전시회를 다녀오셨나요? \n 소중한 경험을 후기로 작성하고 공유해보세요:)"
         />
-        <ReviewEditForm>
-          <Fieldset>
-            <Legend>전시회 이름</Legend>
-            <SearchContainer>
+        <ReviewEditForm layout="vertical">
+          <Form.Item name="exhibitionId" label="다녀 온 전시회">
+            <OuterContainer>
               <InnerContainer>
                 <SearchBar
-                  placeholder="전시회를 검색해 주세요."
-                  allowClear
+                  placeholder="전시회 제목을 검색해 주세요."
                   enterButton
                   onSearch={handleSearch}
+                  ref={searchBarRef}
                 />
                 <ResultList>
                   <ResultItem>서울 핸디아티코리아 2022</ResultItem>
                   <ResultItem>서울 핸디아티코리아 2022</ResultItem>
-                  <ResultItem>서울 핸디아티코리아 2022</ResultItem>
-                  <ResultItem>서울 핸디아티코리아 2022</ResultItem>
-                  <ResultItem>서울 핸디아티코리아 2022</ResultItem>
-                  <ResultItem>서울 핸디아티코리아 2022</ResultItem>
-                  <ResultItem>서울 핸디아티코리아 2022</ResultItem>
-                  <ResultItem>서울 핸디아티코리아 2022</ResultItem>
-                  <ResultItem>서울 핸디아티코리아 2022</ResultItem>
-                  <ResultItem>서울 핸디아티코리아 2022</ResultItem>
-                  <ResultItem>서울 핸디아티코리아 2022</ResultItem>
-                  <ResultItem>서울 핸디아티코리아 2022</ResultItem>
                 </ResultList>
               </InnerContainer>
-              <ImageWrapper>
-                <Image
-                  src="https://www.culture.go.kr/upload/rdf/22/07/show_2022071816261910020.jpg"
-                  alt=""
-                  height="100%"
-                />
-              </ImageWrapper>
-            </SearchContainer>
-          </Fieldset>
+              <Poster
+                src="https://www.culture.go.kr/upload/rdf/22/07/show_2022071816261910020.jpg"
+                alt="전시회 포스터 이미지"
+              />
+            </OuterContainer>
+          </Form.Item>
 
-          <Label>다녀 온 날짜</Label>
-          <DateInput />
-          <Label>제목</Label>
+          <Form.Item name="date" label="다녀 온 날짜">
+            <DateInput onChange={handleDateChange} />
+          </Form.Item>
+
+          <Form.Item name="title" label="제목">
+            <Input placeholder="제목을 입력해주세요." showCount maxLength={30} />
+          </Form.Item>
+
+          <Form.Item name="content" label="내용">
+            <TextArea placeholder="내용을 입력해주세요." autoSize />
+          </Form.Item>
+
+          <Form.Item name="isPublic" label="공개 여부">
+            <ToggleSwitch defaultChecked onChange={handleSwitchChange} />
+            {isPublic ? '전체 공개' : '비공개'}
+          </Form.Item>
+
+          {/* <Label>제목</Label>
           <Input placeholder="제목을 입력해주세요." showCount maxLength={30} />
           <Label>내용</Label>
           <TextArea placeholder="내용을 입력해주세요." autoSize />
@@ -72,9 +91,11 @@ const ReviewCreatePage = () => {
           <ImageUpload />
           <Label>공개 여부</Label>
           <ToggleSwitch defaultChecked onChange={handleSwitchChange} />
-          {isPublic ? '전체 공개' : '비공개'}
+          {isPublic ? '전체 공개' : '비공개'} */}
 
-          <SubmitButton type="primary">작성완료</SubmitButton>
+          <SubmitButton type="primary" htmlType="submit">
+            작성완료
+          </SubmitButton>
         </ReviewEditForm>
       </Section>
     </>
@@ -87,7 +108,7 @@ const Section = styled.section`
   font-size: 1.8rem;
 `;
 
-const ReviewEditForm = styled.form`
+const ReviewEditForm = styled(Form)`
   display: flex;
   flex-direction: column;
   margin: 50px 0;
@@ -100,7 +121,7 @@ const Legend = styled.legend`
   font-weight: bold;
 `;
 
-const SearchContainer = styled.div`
+const OuterContainer = styled.div`
   width: 100%;
   display: flex;
 `;
@@ -132,6 +153,11 @@ const ResultList = styled.ul`
 const ResultItem = styled.li`
   font-size: 1.6rem;
   cursor: pointer;
+`;
+
+const Poster = styled(Image)`
+  width: 150px;
+  flex-shrink: 0;
 `;
 
 const ImageWrapper = styled.div`
