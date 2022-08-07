c8828deb95cfda5dc190c52803c8b70d4f9c3906
Daeun Kim <o_o___t@pusan.ac.kr>
2022-08-07 23:37:57 +0900

    fix: CommentWriteProps 속성 변경

--- components/organism/CommentWrite/index.tsx
+++ components/organism/CommentWrite/index.tsx
@@ -4,8 +4,18 @@ import { UserAvatar, NotLoginAvatar } from 'components/atom';
 import { useRef, useState } from 'react';
 import { UserProps } from 'types/model';
 import { useClickAway } from 'hooks';
+import router from 'next/router';
 
-const CommentWrite = ({ user }: { user: UserProps | null | undefined }) => {
+interface CommentWriteProps {
+  user?: UserProps;
+  parentId?: number;
+  handleReplyCancel?: () => void;
+}
+
+// TODO: 유저의 로그인 상태는 전역 관리.
+// 우선은 props로 받지만, 전역 관리 로직이 완료되면
+// 컴포넌트 안에서 관리하도록 한다.
+const CommentWrite = ({ user, parentId, handleReplyCancel }: CommentWriteProps) => {
   const [show, setShow] = useState(false);
   const [comment, setComment] = useState('');
   const commentContainerRef = useRef<HTMLDivElement>(null);
@@ -17,6 +27,8 @@ const CommentWrite = ({ user }: { user: UserProps | null | undefined }) => {
   const handleCancel = () => {
     setComment('');
     setShow(false);
+    handleReplyCancel && handleReplyCancel();
+    // 답글의 경우 핸들하는 함수
   };
 
   const clickAwayHandler = () => {
@@ -27,7 +39,13 @@ const CommentWrite = ({ user }: { user: UserProps | null | undefined }) => {
 
   const handleOnClick = () => {
     // TODO: 댓글 작성 api 호출하는 로직
-    console.log('댓글 작성 api 호출');
+    // TODO: 댓글과 답글 로직을 구분, 답글의 경우, parentId(commentId)를 받음
+    if (parentId) {
+      console.log('답글 작성 api 호출');
+    } else {
+      console.log('댓글 작성 api 호출');
+    }
+
     console.log('보낼 content', comment);
     setShow(false);
     setComment('');
@@ -35,7 +53,11 @@ const CommentWrite = ({ user }: { user: UserProps | null | undefined }) => {
 
   {
     return !user ? (
-      <CommentWriteContainer>
+      <CommentWriteContainer
+        onClick={() => {
+          router.push('/signin');
+        }}
+      >
         <NotLoginAvatar size={36} />
         <CommentInput placeholder="로그인해야 댓글을 작성할 수 있습니다." disabled />
       </CommentWriteContainer>
