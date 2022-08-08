b8f4aa0381c93e3a85d45c55dd8336f4ff3e4e98
Hyevvy <hyeg0@naver.com>
2022-08-08 15:30:18 +0900

    fix: ret 못 찾는 오류 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,88 @@ const Home: NextPage = () => {
   const [selectedArea, setSelectedArea] = useState([]);
   const [selectedPeriod, setSelectedPeriod] = useState([]);
 
+  const ret = {
+    exhibitionId: 1,
+    name: '번아웃증후군',
+    thumbnail: 'https://www.culture.go.kr/upload/rdf/22/07/show_2022071816261910020.jpg',
+    startDate: '2022-08-04',
+    endDate: '2022-08-10',
+    area: 'SEOUL',
+    url: 'http://galleryraon.com/?page_id=2472#upcoming',
+    placeUrl: 'http://galleryraon.com/?page_id=2472#upcoming',
+    inquiry: '010-8425-8082',
+    fee: '무료',
+    genre: '',
+    description: '',
+    likeCount: 5,
+    placeAddr: '서울특별시 종로구 자하문로41길 4 갤러리라온',
+    lat: 37.597625,
+    lng: 126.962292,
+    isLiked: false,
+    reviews: [
+      {
+        reviewId: 111,
+        user: {
+          userId: 11,
+          profileImage: 'https~',
+          nickname: 'Emily',
+        },
+        title: '번아웃증후군 전시회 다녀옴~',
+        content: '오늘 번아웃증후군 전시회를 다녀왔다. 정말 재밌었다~~',
+        createdAt: '2022-03-22T22:33:11',
+        isLiked: false,
+        likeCount: 32,
+        commentCount: 2,
+        photos: ['https~', 'https~'],
+      },
+      {
+        reviewId: 111,
+        user: {
+          userId: 11,
+          profileImage: 'https~',
+          nickname: 'Emily',
+        },
+        title: '번아웃증후군 전시회 다녀옴~',
+        content: '오늘 번아웃증후군 전시회를 다녀왔다. 정말 재밌었다~~',
+        createdAt: '2022-03-22T22:33:11',
+        isLiked: false,
+        likeCount: 32,
+        commentCount: 2,
+        photos: ['https~', 'https~'],
+      },
+      {
+        reviewId: 111,
+        user: {
+          userId: 11,
+          profileImage: 'https~',
+          nickname: 'Emily',
+        },
+        title: '번아웃증후군 전시회 다녀옴~',
+        content: '오늘 번아웃증후군 전시회를 다녀왔다. 정말 재밌었다~~',
+        createdAt: '2022-03-22T22:33:11',
+        isLiked: false,
+        likeCount: 32,
+        commentCount: 2,
+        photos: ['https~', 'https~'],
+      },
+      {
+        reviewId: 111,
+        user: {
+          userId: 11,
+          profileImage: 'https~',
+          nickname: 'Emily',
+        },
+        title: '번아웃증후군 전시회 다녀옴~',
+        content: '오늘 번아웃증후군 전시회를 다녀왔다. 정말 재밌었다~~',
+        createdAt: '2022-03-22T22:33:11',
+        isLiked: false,
+        likeCount: 32,
+        commentCount: 2,
+        photos: ['https~', 'https~'],
+      },
+    ],
+  };
+
   return (
     <div>
       <SwiperContainer title="다가오는 전시회" type="upcoming">
@@ -23,7 +105,7 @@ const Home: NextPage = () => {
               likeCount={ret.likeCount}
               reviewCount={ret.reviews.length}
               isLiked={true}
-              key={ret.exhibitionId}
+              key={1}
             />,
             <ExhibitionCard
               exhibitionId={ret.exhibitionId}
@@ -34,7 +116,7 @@ const Home: NextPage = () => {
               likeCount={ret.likeCount}
               reviewCount={ret.reviews.length}
               isLiked={ret.isLiked}
-              key={ret.exhibitionId}
+              key={2}
             />,
             <ExhibitionCard
               exhibitionId={ret.exhibitionId}
@@ -45,7 +127,7 @@ const Home: NextPage = () => {
               likeCount={ret.likeCount}
               reviewCount={ret.reviews.length}
               isLiked={false}
-              key={ret.exhibitionId}
+              key={3}
             />,
             <ExhibitionCard
               exhibitionId={ret.exhibitionId}
@@ -56,7 +138,7 @@ const Home: NextPage = () => {
               likeCount={ret.likeCount}
               reviewCount={ret.reviews.length}
               isLiked={false}
-              key={ret.exhibitionId}
+              key={4}
             />,
             <ExhibitionCard
               exhibitionId={ret.exhibitionId}
@@ -67,7 +149,7 @@ const Home: NextPage = () => {
               likeCount={ret.likeCount}
               reviewCount={ret.reviews.length}
               isLiked={ret.isLiked}
-              key={ret.exhibitionId}
+              key={5}
             />,
             <ExhibitionCard
               exhibitionId={ret.exhibitionId}
@@ -78,7 +160,7 @@ const Home: NextPage = () => {
               likeCount={ret.likeCount}
               reviewCount={ret.reviews.length}
               isLiked={ret.isLiked}
-              key={ret.exhibitionId}
+              key={6}
             />,
             <ExhibitionCard
               exhibitionId={ret.exhibitionId}
@@ -89,7 +171,7 @@ const Home: NextPage = () => {
               likeCount={ret.likeCount}
               reviewCount={ret.reviews.length}
               isLiked={ret.isLiked}
-              key={ret.exhibitionId}
+              key="7"
             />,
             <ExhibitionCard
               exhibitionId={ret.exhibitionId}
@@ -100,7 +182,7 @@ const Home: NextPage = () => {
               likeCount={ret.likeCount}
               reviewCount={ret.reviews.length}
               isLiked={ret.isLiked}
-              key={ret.exhibitionId}
+              key={8}
             />,
             <ExhibitionCard
               exhibitionId={ret.exhibitionId}
@@ -111,7 +193,7 @@ const Home: NextPage = () => {
               likeCount={ret.likeCount}
               reviewCount={ret.reviews.length}
               isLiked={ret.isLiked}
-              key={ret.exhibitionId}
+              key={9}
             />,
           ]}
         />
@@ -129,7 +211,7 @@ const Home: NextPage = () => {
               likeCount={ret.likeCount}
               reviewCount={ret.reviews.length}
               isLiked={ret.isLiked}
-              key={ret.exhibitionId}
+              key={1}
             />,
             <ExhibitionCard
               exhibitionId={ret.exhibitionId}
@@ -140,7 +222,7 @@ const Home: NextPage = () => {
               likeCount={ret.likeCount}
               reviewCount={ret.reviews.length}
               isLiked={ret.isLiked}
-              key={ret.exhibitionId}
+              key={2}
             />,
             <ExhibitionCard
               exhibitionId={ret.exhibitionId}
@@ -151,7 +233,7 @@ const Home: NextPage = () => {
               likeCount={ret.likeCount}
               reviewCount={ret.reviews.length}
               isLiked={ret.isLiked}
-              key={ret.exhibitionId}
+              key={3}
             />,
             <ExhibitionCard
               exhibitionId={ret.exhibitionId}
@@ -162,7 +244,7 @@ const Home: NextPage = () => {
               likeCount={ret.likeCount}
               reviewCount={ret.reviews.length}
               isLiked={ret.isLiked}
-              key={ret.exhibitionId}
+              key={4}
             />,
             <ExhibitionCard
               exhibitionId={ret.exhibitionId}
@@ -173,51 +255,7 @@ const Home: NextPage = () => {
               likeCount={ret.likeCount}
               reviewCount={ret.reviews.length}
               isLiked={ret.isLiked}
-              key={ret.exhibitionId}
-            />,
-            <ExhibitionCard
-              exhibitionId={ret.exhibitionId}
-              name={ret.name}
-              thumbnail={ret.thumbnail}
-              startDate={ret.startDate}
-              endDate={ret.endDate}
-              likeCount={ret.likeCount}
-              reviewCount={ret.reviews.length}
-              isLiked={ret.isLiked}
-              key={ret.exhibitionId}
-            />,
-            <ExhibitionCard
-              exhibitionId={ret.exhibitionId}
-              name={ret.name}
-              thumbnail={ret.thumbnail}
-              startDate={ret.startDate}
-              endDate={ret.endDate}
-              likeCount={ret.likeCount}
-              reviewCount={ret.reviews.length}
-              isLiked={ret.isLiked}
-              key={ret.exhibitionId}
-            />,
-            <ExhibitionCard
-              exhibitionId={ret.exhibitionId}
-              name={ret.name}
-              thumbnail={ret.thumbnail}
-              startDate={ret.startDate}
-              endDate={ret.endDate}
-              likeCount={ret.likeCount}
-              reviewCount={ret.reviews.length}
-              isLiked={ret.isLiked}
-              key={ret.exhibitionId}
-            />,
-            <ExhibitionCard
-              exhibitionId={ret.exhibitionId}
-              name={ret.name}
-              thumbnail={ret.thumbnail}
-              startDate={ret.startDate}
-              endDate={ret.endDate}
-              likeCount={ret.likeCount}
-              reviewCount={ret.reviews.length}
-              isLiked={ret.isLiked}
-              key={ret.exhibitionId}
+              key={5}
             />,
           ]}
         />
