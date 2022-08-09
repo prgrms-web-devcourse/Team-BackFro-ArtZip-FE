7e20c490bc86ee23f13821a0d64888407e4f49e5
yunjjeongjo <two6885@gmail.com>
2022-08-10 01:12:30 +0900

    feat: 만료기간 설정

--- utils/storage/index.ts
+++ utils/storage/index.ts
@@ -1,11 +1,16 @@
 const storage = {
   getItem: <T>(key: string, initialValue: T) => {
     const value = localStorage.getItem(key);
-    return value ? JSON.parse(value) : initialValue;
+    return value ? JSON.parse(value).value : initialValue;
   },
   setItem: <T>(key: string, value: T) => {
+    const expireDate = new Date();
     try {
-      localStorage.setItem(key, JSON.stringify(value));
+      const obj = {
+        value: value,
+        expire: expireDate.setMinutes(expireDate.getMinutes() + 30),
+      };
+      localStorage.setItem(key, JSON.stringify(obj));
     } catch (error) {
       console.error(error);
     }
