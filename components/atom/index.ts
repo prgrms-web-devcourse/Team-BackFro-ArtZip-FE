b53b88c190ddfe6baa26d0e74b83e1bedeb28dff
Daeun Kim <74234333+dar-jeeling@users.noreply.github.com>
2022-08-09 20:02:37 +0900

    feat: 댓글 컴포넌트 구현 (#122)
    * refactor: userAvatar 컴포넌트 분리
    
    * feat: 로그인 하지 않은 유저 아바타 컴포넌트 구현
    
    * feat: useClickAway 훅 구현
    
    * feat: CommentWrite 컴포넌트 구현
    
    * fix: comment API 타입 수정
    
    * test: 댓글 mock 데이터 변경
    
    * feat: CommentList 컴포넌트 구현
    
    * design: 일부 css 속성 변경
    
    * fix: CommentWriteProps 속성 변경
    
    * feat: CommentUtils 컴포넌트 구현
    
    * refactor: 주석 제거
    
    * test: mock 데이터 변경
    
    * design: antd Comment 컴포넌트 커스텀

--- components/atom/index.ts
+++ components/atom/index.ts
@@ -1,2 +1,4 @@
 export { default as LikeToggle } from './LikeToggle';
 export { default as LinkButton } from './LinkButton';
+export { default as UserAvatar } from './UserAvatar';
+export { default as NotLoginAvatar } from './NotLoginAvatar';
