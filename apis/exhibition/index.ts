f3a2ae42cd681e571cfed1f3c18fdf7b41177489
Hyevvy <hyeg0@naver.com>
2022-08-11 01:21:16 +0900

    feat: 맞춤페이지 서버통신 구현

--- apis/exhibition/index.ts
+++ apis/exhibition/index.ts
@@ -17,6 +17,24 @@ const exhibitionAPI = {
       `/api/v1/exhibitions?query=${query}&page=${page}&size=${size}&include-end=${includeEnd}`,
     );
   },
+  custom: (area?: string, month?: string, page?: number, size?: number) => {
+    // if (area!.length === 0 && month!.length === 0) {
+    //   return unAuthRequest.get(`/api/v1/exhibitions/custom?page=${page}&size=${size}`);
+    // }
+    // if (area!.length === 0) {
+    //   return unAuthRequest.get(
+    //     `/api/v1/exhibitions/custom?months=${month}&page=${page}&size=${size}`,
+    //   );
+    // }
+    // if (month!.length === 0) {
+    //   return unAuthRequest.get(
+    //     `/api/v1/exhibitions/custom?areas=${area}&page=${page}&size=${size}`,
+    //   );
+    // }
+    return unAuthRequest.get(
+      `/api/v1/exhibitions/custom?areas=${area}&months=${month}&page=${page}&size=${size}`,
+    );
+  },
 };
 
 export default exhibitionAPI;
