ccd65178f17c5365a86f52d8657cd96d3d005999
Hyevvy <72402747+Hyevvy@users.noreply.github.com>
2022-08-10 19:58:52 +0900

    feat: 메인페이지 서버통신 구현 (#143)
    * feat: 메인페이지 서버통신 구현
    
    * comment: 불필요한 주석 삭제
    
    * comment: 불필요한 주석 삭제

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,259 +4,31 @@ import SwiperWrapper from 'components/organism/Swiper';
 import SwiperContainer from 'components/organism/SwiperContainer';
 import type { NextPage } from 'next';
 import { homeStyle as S } from '../styles/pages';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { exhibitionAPI } from 'apis';
+import { ExhibitionProps } from 'types/model';
 
 const Home: NextPage = () => {
-  const ret = {
-    exhibitionId: 1,
-    name: '번아웃증후군',
-    thumbnail: 'https://www.culture.go.kr/upload/rdf/22/07/show_2022071816261910020.jpg',
-    startDate: '2022-08-04',
-    endDate: '2022-08-10',
-    area: 'SEOUL',
-    url: 'http://galleryraon.com/?page_id=2472#upcoming',
-    placeUrl: 'http://galleryraon.com/?page_id=2472#upcoming',
-    inquiry: '010-8425-8082',
-    fee: '무료',
-    genre: '',
-    description: '',
-    likeCount: 5,
-    placeAddr: '서울특별시 종로구 자하문로41길 4 갤러리라온',
-    lat: 37.597625,
-    lng: 126.962292,
-    isLiked: false,
-    reviews: [
-      {
-        reviewId: 111,
-        user: {
-          userId: 11,
-          profileImage: 'https~',
-          nickname: 'Emily',
-        },
-        title: '번아웃증후군 전시회 다녀옴~',
-        content: '오늘 번아웃증후군 전시회를 다녀왔다. 정말 재밌었다~~',
-        createdAt: '2022-03-22T22:33:11',
-        isLiked: false,
-        likeCount: 32,
-        commentCount: 2,
-        photos: ['https~', 'https~'],
-      },
-      {
-        reviewId: 111,
-        user: {
-          userId: 11,
-          profileImage: 'https~',
-          nickname: 'Emily',
-        },
-        title: '번아웃증후군 전시회 다녀옴~',
-        content: '오늘 번아웃증후군 전시회를 다녀왔다. 정말 재밌었다~~',
-        createdAt: '2022-03-22T22:33:11',
-        isLiked: false,
-        likeCount: 32,
-        commentCount: 2,
-        photos: ['https~', 'https~'],
-      },
-      {
-        reviewId: 111,
-        user: {
-          userId: 11,
-          profileImage: 'https~',
-          nickname: 'Emily',
-        },
-        title: '번아웃증후군 전시회 다녀옴~',
-        content: '오늘 번아웃증후군 전시회를 다녀왔다. 정말 재밌었다~~',
-        createdAt: '2022-03-22T22:33:11',
-        isLiked: false,
-        likeCount: 32,
-        commentCount: 2,
-        photos: ['https~', 'https~'],
-      },
-      {
-        reviewId: 111,
-        user: {
-          userId: 11,
-          profileImage: 'https~',
-          nickname: 'Emily',
-        },
-        title: '번아웃증후군 전시회 다녀옴~',
-        content: '오늘 번아웃증후군 전시회를 다녀왔다. 정말 재밌었다~~',
-        createdAt: '2022-03-22T22:33:11',
-        isLiked: false,
-        likeCount: 32,
-        commentCount: 2,
-        photos: ['https~', 'https~'],
-      },
-    ],
-  };
+  const [upcomingExhibitions, setUpcomingExhibitions] = useState<ExhibitionProps[]>([]);
+  const [mostLikeExhibitions, setMostLikeExhibitions] = useState<ExhibitionProps[]>([]);
 
+  useEffect(() => {
+    exhibitionAPI.getUpcoming(0, 8).then((res) => {
+      setUpcomingExhibitions(res.data.data.content);
+    });
+
+    exhibitionAPI.getMostLike(0, 8, true).then((res) => {
+      setMostLikeExhibitions(res.data.data.content);
+    });
+  }, []);
   return (
     <div>
       <SwiperContainer title="다가오는 전시회" type="upcoming">
-        <SwiperWrapper
-          items={[
-            <ExhibitionCard
-              exhibitionId={ret.exhibitionId}
-              name={ret.name}
-              thumbnail={ret.thumbnail}
-              startDate={ret.startDate}
-              endDate={ret.endDate}
-              likeCount={ret.likeCount}
-              reviewCount={ret.reviews.length}
-              isLiked={true}
-              key={1}
-            />,
-            <ExhibitionCard
-              exhibitionId={ret.exhibitionId}
-              name={ret.name}
-              thumbnail={ret.thumbnail}
-              startDate={ret.startDate}
-              endDate={ret.endDate}
-              likeCount={ret.likeCount}
-              reviewCount={ret.reviews.length}
-              isLiked={ret.isLiked}
-              key={2}
-            />,
-            <ExhibitionCard
-              exhibitionId={ret.exhibitionId}
-              name={ret.name}
-              thumbnail={ret.thumbnail}
-              startDate={ret.startDate}
-              endDate={ret.endDate}
-              likeCount={ret.likeCount}
-              reviewCount={ret.reviews.length}
-              isLiked={false}
-              key={3}
-            />,
-            <ExhibitionCard
-              exhibitionId={ret.exhibitionId}
-              name={ret.name}
-              thumbnail={ret.thumbnail}
-              startDate={ret.startDate}
-              endDate={ret.endDate}
-              likeCount={ret.likeCount}
-              reviewCount={ret.reviews.length}
-              isLiked={false}
-              key={4}
-            />,
-            <ExhibitionCard
-              exhibitionId={ret.exhibitionId}
-              name={ret.name}
-              thumbnail={ret.thumbnail}
-              startDate={ret.startDate}
-              endDate={ret.endDate}
-              likeCount={ret.likeCount}
-              reviewCount={ret.reviews.length}
-              isLiked={ret.isLiked}
-              key={5}
-            />,
-            <ExhibitionCard
-              exhibitionId={ret.exhibitionId}
-              name={ret.name}
-              thumbnail={ret.thumbnail}
-              startDate={ret.startDate}
-              endDate={ret.endDate}
-              likeCount={ret.likeCount}
-              reviewCount={ret.reviews.length}
-              isLiked={ret.isLiked}
-              key={6}
-            />,
-            <ExhibitionCard
-              exhibitionId={ret.exhibitionId}
-              name={ret.name}
-              thumbnail={ret.thumbnail}
-              startDate={ret.startDate}
-              endDate={ret.endDate}
-              likeCount={ret.likeCount}
-              reviewCount={ret.reviews.length}
-              isLiked={ret.isLiked}
-              key="7"
-            />,
-            <ExhibitionCard
-              exhibitionId={ret.exhibitionId}
-              name={ret.name}
-              thumbnail={ret.thumbnail}
-              startDate={ret.startDate}
-              endDate={ret.endDate}
-              likeCount={ret.likeCount}
-              reviewCount={ret.reviews.length}
-              isLiked={ret.isLiked}
-              key={8}
-            />,
-            <ExhibitionCard
-              exhibitionId={ret.exhibitionId}
-              name={ret.name}
-              thumbnail={ret.thumbnail}
-              startDate={ret.startDate}
-              endDate={ret.endDate}
-              likeCount={ret.likeCount}
-              reviewCount={ret.reviews.length}
-              isLiked={ret.isLiked}
-              key={9}
-            />,
-          ]}
-        />
+        <SwiperWrapper items={upcomingExhibitions} />
       </SwiperContainer>
 
       <SwiperContainer title="인기많은 전시회" type="popular">
-        <SwiperWrapper
-          items={[
-            <ExhibitionCard
-              exhibitionId={ret.exhibitionId}
-              name={ret.name}
-              thumbnail={ret.thumbnail}
-              startDate={ret.startDate}
-              endDate={ret.endDate}
-              likeCount={ret.likeCount}
-              reviewCount={ret.reviews.length}
-              isLiked={ret.isLiked}
-              key={1}
-            />,
-            <ExhibitionCard
-              exhibitionId={ret.exhibitionId}
-              name={ret.name}
-              thumbnail={ret.thumbnail}
-              startDate={ret.startDate}
-              endDate={ret.endDate}
-              likeCount={ret.likeCount}
-              reviewCount={ret.reviews.length}
-              isLiked={ret.isLiked}
-              key={2}
-            />,
-            <ExhibitionCard
-              exhibitionId={ret.exhibitionId}
-              name={ret.name}
-              thumbnail={ret.thumbnail}
-              startDate={ret.startDate}
-              endDate={ret.endDate}
-              likeCount={ret.likeCount}
-              reviewCount={ret.reviews.length}
-              isLiked={ret.isLiked}
-              key={3}
-            />,
-            <ExhibitionCard
-              exhibitionId={ret.exhibitionId}
-              name={ret.name}
-              thumbnail={ret.thumbnail}
-              startDate={ret.startDate}
-              endDate={ret.endDate}
-              likeCount={ret.likeCount}
-              reviewCount={ret.reviews.length}
-              isLiked={ret.isLiked}
-              key={4}
-            />,
-            <ExhibitionCard
-              exhibitionId={ret.exhibitionId}
-              name={ret.name}
-              thumbnail={ret.thumbnail}
-              startDate={ret.startDate}
-              endDate={ret.endDate}
-              likeCount={ret.likeCount}
-              reviewCount={ret.reviews.length}
-              isLiked={ret.isLiked}
-              key={5}
-            />,
-          ]}
-        />
+        <SwiperWrapper items={mostLikeExhibitions} />
       </SwiperContainer>
 
       <S.MapContainer>
