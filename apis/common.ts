17a4bf3956c5e439aaa5544b6201e3979651dd9c
Daeun Kim <o_o___t@pusan.ac.kr>
2022-08-12 02:10:39 +0900

    feat: authRequest axios 설정

--- apis/common.ts
+++ apis/common.ts
@@ -1,6 +1,7 @@
+import { userAPI } from 'apis';
 import axios from 'axios';
-import { refresh, refreshErrorHandle } from './refresh';
-
+import { parseJwt } from 'utils';
+import cookie from 'react-cookies';
 const baseURL = `${process.env.NEXT_PUBLIC_API_END_POINT}`;
 
 const authRequest = axios.create({
@@ -11,6 +12,39 @@ const unAuthRequest = axios.create({
   baseURL,
 });
 
-authRequest.interceptors.request.use(refresh, refreshErrorHandle);
+authRequest.interceptors.request.use((config) => {
+  if (config.headers) {
+    config.headers.accessToken = cookie.load('ACCESS_TOKEN');
+    return config;
+  }
+});
+
+authRequest.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async function (error) {
+    const originalRequest = error.config;
+    const UNAUTHORIZE_CODE = 'U005';
+    if (
+      error.response.status === 401 ||
+      (error.response.status === 403 && error.response.data.code === UNAUTHORIZE_CODE)
+    ) {
+      originalRequest._retry = true;
+      const accessToken = cookie.load('ACCESS_TOKEN');
+      const refreshToken = cookie.load('REFRESH_TOKEN');
+      const { userId } = parseJwt(accessToken);
+      const tokenRequestBody = {
+        userId,
+        accessToken,
+        refreshToken,
+      };
+      const newAccessToken = await userAPI.reissueToken(tokenRequestBody);
+      originalRequest.headers.accessToken = newAccessToken;
+      return authRequest(originalRequest);
+    }
+    return Promise.reject(error);
+  },
+);
 
 export { authRequest, unAuthRequest };
