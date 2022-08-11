b0a6bea6469d401e07282d5553a011821706c2e6
gitul0515 <gitul0515@gmail.com>
2022-08-11 21:15:46 +0900

    feat: 유저 프로필 디폴트 이미지 추가

--- constants/imageUrl.ts
+++ constants/imageUrl.ts
@@ -1,4 +1,6 @@
 const imageUrl = {
+  USER_DEFAULT:
+    'https://devcourse-backfro-s3.s3.ap-northeast-2.amazonaws.com/profileImage/default/anonymous-user.jpg',
   EXHIBITION_DEFAULT:
     'https://raw.githubusercontent.com/gitul0515/gitul0515.github.io/main/_posts/image/poster-default-image.png',
 };
