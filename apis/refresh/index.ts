9ee3736ba2c01a3c15c8d5e29a83423bf094c95f
Daeun Kim <o_o___t@pusan.ac.kr>
2022-08-11 17:27:55 +0900

    fix: top level에서 hook 을 사용하는 코드 변경

--- apis/refresh/index.ts
+++ apis/refresh/index.ts
@@ -1,18 +1,28 @@
 import { userAPI } from 'apis';
 import { AxiosRequestConfig } from 'axios';
 import moment from 'moment';
-import { useRecoilValue } from 'recoil';
-import { userAtom } from 'states';
 import { cookie, storage } from 'utils';
+
+function parseJwt(token: string) {
+  const base64Url = token.split('.')[1];
+  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  const jsonPayload = decodeURIComponent(
+    window
+      .atob(base64)
+      .split('')
+      .map(function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join(''),
+  );
+
+  return JSON.parse(jsonPayload);
+}
 
 const refresh = async (config: AxiosRequestConfig): Promise<AxiosRequestConfig> => {
   const currentRefreshToken = cookie.getItem('REFRESH_TOKEN', '');
   const { value: currentAccessToken, expire: expireAt } = storage.getItem('ACCESS_TOKEN', '');
-
-  // TODO: api 준비되면 구현이 살짝 바뀜
-
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const userId = useRecoilValue(userAtom);
+  const userId = parseJwt(currentAccessToken.userId);
 
   // 토큰이 만료되었고, refreshToken 이 저장되어 있을 때 + 유저가 로그인 했을 때
   if (userId && moment(expireAt).diff(moment()) < 0 && currentRefreshToken) {
@@ -38,6 +48,7 @@ const refresh = async (config: AxiosRequestConfig): Promise<AxiosRequestConfig> 
   }
 
   // 토큰이 만료되지 않은 경우
+  storage.setItem('userTest', userId);
   config.headers.accessToken = currentAccessToken;
   return config;
 };
