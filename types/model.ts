088009ca28f0d8d515a70785264e9b5462f7c74a
yunjjeongjo <two6885@gmail.com>
2022-08-12 05:10:04 +0900

    feat: ReviewProps 속성 추가

--- types/model.ts
+++ types/model.ts
@@ -27,6 +27,11 @@ export interface ReviewProps {
   isPublic: boolean;
   commentCount: number;
   isLiked: boolean;
+  title: string;
+  content: string;
+  photos: PhotoProps[] | null;
+  createdAt: string;
+  user: UserProps;
 }
 
 // TODO: 명세서 올라오면 수정하기
