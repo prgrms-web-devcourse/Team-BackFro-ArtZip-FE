c705e9b7fb793794188651d9e6e802da926f542e
gitul0515 <gitul0515@gmail.com>
2022-08-12 04:18:32 +0900

    feat: 유저 페이지(기본 정보) 서버 연동

--- pages/users/[id]/index.tsx
+++ pages/users/[id]/index.tsx
@@ -1,44 +1,28 @@
 import styled from '@emotion/styled';
-import { Tabs, Image } from 'antd';
+import { Tabs, Image, Spin } from 'antd';
 import { ReviewCard, ExhibitionCard, SideNavigation } from 'components/molecule';
 import { userAPI } from 'apis';
-import { GetServerSideProps, GetStaticProps } from 'next';
-import { UserInfoResponse } from 'types/apis/user';
-import { useEffect, useState } from 'react';
-import { ExhibitionProps } from 'types/model';
-import imageUrl from 'constants/imageUrl';
+import { GetServerSideProps } from 'next';
+import { UserInfoResponse, UserReviewResponse } from 'types/apis/user';
+import { useState } from 'react';
+import { ReviewCardProps, ExhibitionProps } from 'types/model';
 
 interface UserPageProps {
-  userInfo: {
-    nickname: string;
-    userId: number;
-    profileImage: string;
-    email: string;
-    reviewCount: number;
-    reviewLikeCount: number;
-    exhibitionLikeCount: number;
-    commentCount: number;
-  };
-  userExhibitions: {
-    exhibitionId: number;
-    name: string;
-    thumbnail: string;
-    startDate: string;
-    endDate: string;
-    likeCount: number;
-    reviewCount: number;
-    isLiked: boolean;
-  }[];
+  userInfoResponse: UserInfoResponse;
+  userReviewResponse: UserReviewResponse;
 }
 
-const UserPage = ({ userInfo, userExhibitions }: UserPageProps) => {
-  const [myReviews, setMyReviews] = useState();
-  const [likeReviews, setLikeReviews] = useState();
+const UserPage = ({ userInfoResponse, userReviewResponse }: UserPageProps) => {
+  const userInfo = userInfoResponse.data;
+  const userReviews = userReviewResponse.data?.content;
+
+  const [myReviews, setMyReviews] = useState<ReviewCardProps[] | undefined>(userReviews);
+  const [likeReviews, setLikeReviews] = useState<ReviewCardProps[]>();
+  const [likeExhibitions, setLikeExhibitions] = useState<Required<ExhibitionProps>[]>();
 
   const handleTabClick = (key: string) => {
     switch (key) {
       case 'myReview': {
-        fetchMyReviews();
         return;
       }
       case 'likeReview': {
@@ -46,71 +30,110 @@ const UserPage = ({ userInfo, userExhibitions }: UserPageProps) => {
         return;
       }
       case 'likeExhibition': {
+        fetchLikeExhibitions();
         return;
       }
       default:
-        console.error('유효하지 않은 key입니다.');
+        console.error('Invalid key');
     }
   };
 
-  const fetchMyReviews = async () => {
-    const result = await userAPI.getMyReview(userInfo.userId, 0, 10).then((res) => res.data);
-    console.log(result);
-  };
-
   const fetchLikeReviews = async () => {
-    const result = await userAPI.getLikeReview(userInfo.userId, 0, 10).then((res) => res.data);
-    console.log(result);
+    if (userInfo && !likeReviews) {
+      const { content } = await userAPI
+        .getLikeReview(userInfo.userId, 0, 10)
+        .then((res) => res.data.data);
+      setLikeReviews(content);
+    }
   };
 
-  return userInfo && userExhibitions ? (
+  const fetchLikeExhibitions = async () => {
+    if (userInfo && !likeExhibitions) {
+      const { content } = await userAPI
+        .getLikeExhibition(userInfo.userId, 0, 10)
+        .then((res) => res.data.data);
+      setLikeExhibitions(content);
+    }
+  };
+
+  return userInfo && userReviews ? (
     <PageContainer>
       <ProfileContainer>
-        <ProfileImage src={userInfo.profileImage} alt="프로필 이미지" preview={false} />
+        <ProfileImage src={userInfo.profileImage} alt="프로필 이미지" />
         <UserName>{userInfo.nickname}</UserName>
         <UserEmail>{userInfo.email}</UserEmail>
       </ProfileContainer>
       <TabCardContainer type="card" tabPosition="top" centered onTabClick={handleTabClick}>
-        <Tab tab={`작성한 후기 (${userInfo.reviewCount})`} key="myReview"></Tab>
+        <Tab tab={`작성한 후기 (${userInfo.reviewCount})`} key="myReview">
+          <ReviewContainer>
+            {myReviews?.map((review) => (
+              <ReviewCard
+                key={review.reviewId}
+                reviewId={review.reviewId}
+                title={review.title}
+                content={review.content}
+                thumbnail={review.exhibition.thumbnail}
+                createdAt={review.createdAt}
+                likeCount={review.likeCount}
+                commentCount={review.commentCount}
+                photo={review.photos[0]?.path}
+                userId={review.user.userId}
+                nickname={review.user.nickname}
+                profileImage={review.user.profileImage}
+              />
+            ))}
+          </ReviewContainer>
+        </Tab>
         <Tab tab={`좋아하는 후기 (${userInfo.reviewLikeCount})`} key="likeReview">
           <ReviewContainer>
-            <ReviewCard
-              reviewId={reviewDummy.reviewId}
-              thumbnail={reviewDummy.exhibition.thumbnail}
-              title={reviewDummy.title}
-              content={reviewDummy.content}
-              createdAt={reviewDummy.createdAt}
-              likeCount={reviewDummy.likeCount}
-              commentCount={reviewDummy.commentCount}
-              photo={reviewDummy.photos[0]}
-              userId={reviewDummy.user.userId}
-              nickname={reviewDummy.user.nickname}
-              profileImage={reviewDummy.user.profileImage}
-            />
+            {likeReviews ? (
+              likeReviews.map((review) => (
+                <ReviewCard
+                  key={review.reviewId}
+                  reviewId={review.reviewId}
+                  title={review.title}
+                  content={review.content}
+                  thumbnail={review.exhibition.thumbnail}
+                  createdAt={review.createdAt}
+                  likeCount={review.likeCount}
+                  commentCount={review.commentCount}
+                  photo={review.photos[0]?.path}
+                  userId={review.user.userId}
+                  nickname={review.user.nickname}
+                  profileImage={review.user.profileImage}
+                />
+              ))
+            ) : (
+              <Spinner size="large" />
+            )}
           </ReviewContainer>
         </Tab>
         <Tab tab={`좋아하는 전시회 (${userInfo.exhibitionLikeCount})`} key="likeExhibition">
           <ExhibitionContainer>
-            {userExhibitions.map((exhibition) => (
-              <ExhibitionCard
-                key={exhibition.exhibitionId}
-                exhibitionId={exhibition.exhibitionId}
-                name={exhibition.name}
-                thumbnail={exhibition.thumbnail}
-                startDate={exhibition.startDate}
-                endDate={exhibition.endDate}
-                likeCount={exhibition.likeCount}
-                reviewCount={exhibition.reviewCount}
-                isLiked={exhibition.isLiked}
-              />
-            ))}
+            {likeExhibitions ? (
+              likeExhibitions.map((exhibition) => (
+                <ExhibitionCard
+                  key={exhibition.exhibitionId}
+                  exhibitionId={exhibition.exhibitionId}
+                  name={exhibition.name}
+                  thumbnail={exhibition.thumbnail}
+                  startDate={exhibition.startDate}
+                  endDate={exhibition.endDate}
+                  likeCount={exhibition.likeCount}
+                  reviewCount={exhibition.reviewCount}
+                  isLiked={exhibition.isLiked}
+                />
+              ))
+            ) : (
+              <Spinner size="large" />
+            )}
           </ExhibitionContainer>
         </Tab>
       </TabCardContainer>
       <SideNavigation
         paths={[
           {
-            href: `/users/${userInfo.userId}`, // TODO: `/users/${userId}`로 수정
+            href: `/users/${userInfo.userId}`,
             pageName: '사용자 정보',
           },
           {
@@ -125,36 +148,39 @@ const UserPage = ({ userInfo, userExhibitions }: UserPageProps) => {
       />
     </PageContainer>
   ) : (
-    '로딩 중입니다.'
+    <Spinner size="large" />
   );
 };
 
+// TODO: 추후 getStaticProps로 변경 예정 (getStaticPaths도 추가)
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   if (params) {
-    const userInfo = await userAPI.getInformation(Number(params.id)).then((res) => res.data.data);
+    const userInfoResponse = await userAPI
+      .getInformation(Number(params.id))
+      .then((res) => res.data);
 
-    const userExhibitions = await userAPI
-      .getLikeExhibition(Number(params.id), 0, 10)
-      .then((res) => res.data.data.content);
+    const userReviewResponse = await userAPI
+      .getMyReview(Number(params.id), 0, 10)
+      .then((res) => res.data);
 
     return {
       props: {
-        userInfo,
-        userExhibitions,
+        userInfoResponse,
+        userReviewResponse,
       },
     };
   }
   return {
     props: {
-      userInfo: {},
-      userExhibitions: {},
+      userInfoResponse: {},
+      userReviewResponse: {},
     },
   };
 };
 
 const PageContainer = styled.div`
   position: relative;
-  max-width: 1000px;
+  max-width: 1100px;
   margin: 0 auto;
   text-align: center;
 `;
@@ -205,7 +231,9 @@ const ReviewContainer = styled.div`
 const ExhibitionContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
+  justify-content: center;
   justify-items: center;
+  gap: 10px;
   padding-bottom: 30px;
 
   @media screen and (max-width: ${({ theme }) => theme.breakPoint.tablet}) {
@@ -217,35 +245,8 @@ const ExhibitionContainer = styled.div`
   }
 `;
 
-const reviewDummy = {
-  reviewId: 43,
-  user: {
-    userId: 11,
-    profileImage: 'https~',
-    nickname: 'Emily',
-  },
-  exhibition: {
-    exhibitionId: 24,
-    name: '전시회 이름',
-    startDate: '2022-10-11',
-    thumbnail: 'https~~',
-  },
-  title: '번아웃증후군 전시회 다녀옴~',
-  content:
-    '오늘 핸드아트코리아 전시회를 다녀왔다. 정말 재밌었다~~ 오늘 핸드아트코리아 전시회를 다녀왔다. 정말 재밌었다~~',
-  createdAt: '2022-03-22T22:33:11',
-  updatedAt: '2022-03-23T13:03:51',
-  isEdited: true,
-  isLiked: false,
-  isPublic: true,
-  likeCount: 32,
-  commentCount: 2,
-  photos: ['https~', 'https~'],
-};
-
-const exhibitionCardStyle = {
-  width: '240px',
-  height: '350px',
-};
+const Spinner = styled(Spin)`
+  margin-bottom: 400px;
+`;
 
 export default UserPage;
