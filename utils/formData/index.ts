cc81d0944af722a3b99ac9c549c1ed7ee8964660
gitul0515 <gitul0515@gmail.com>
2022-08-13 04:07:54 +0900

    refactor: formData 변환 함수가 key의 이름을 받을 수 있도록 수정

--- utils/formData/index.ts
+++ utils/formData/index.ts
@@ -1,18 +1,22 @@
 import type { UploadFile } from 'antd';
 
-export const objectToFormData = <T>(object: T, _formData?: FormData) => {
+export const objectToFormData = <T>(key: string, value: T, _formData?: FormData) => {
   const formData = _formData ? _formData : new FormData();
-  formData.append('data', new Blob([JSON.stringify({ ...object })], { type: 'application/json' }));
+  formData.append(key, new Blob([JSON.stringify({ ...value })], { type: 'application/json' }));
   return formData;
 };
 
-export const filesToFormData = (files: FileList | UploadFile[], _formData?: FormData) => {
+export const filesToFormData = (
+  key: string,
+  files: FileList | UploadFile[],
+  _formData?: FormData,
+) => {
   const formData = _formData ? _formData : new FormData();
   for (let i = 0; i < files.length; i++) {
     if ('originFileObj' in files[i]) {
-      formData.append('files', (files[i] as UploadFile).originFileObj as File); // TODO: 추후 files도 인자로 받을 예정
+      formData.append(key, (files[i] as UploadFile).originFileObj as File);
     } else {
-      formData.append('files', files[i] as File);
+      formData.append(key, files[i] as File);
     }
   }
   return formData;
