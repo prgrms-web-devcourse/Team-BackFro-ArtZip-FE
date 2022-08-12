eece5186b8243f131bf560cf8057442e7b26dda0
Daeun Kim <o_o___t@pusan.ac.kr>
2022-08-12 16:29:26 +0900

    refactor: 사용하지 않는 import 제거

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/global/globals.css';
 import type { AppContext, AppProps } from 'next/app';
-import { RecoilRoot, useRecoilState } from 'recoil';
+import { RecoilRoot } from 'recoil';
 import { Layout } from 'components/template';
 import { ThemeProvider } from '@emotion/react';
 import theme from 'styles/global/theme';
@@ -8,11 +8,9 @@ import 'swiper/scss';
 import 'swiper/scss/navigation';
 import 'swiper/scss/pagination';
 import cookies from 'next-cookies';
-import { setToken } from 'utils';
 import { authRequest } from 'apis/common';
 import cookie from 'react-cookies';
 import App from 'next/app';
-import { userAPI } from 'apis';
 declare global {
   interface Window {
     // eslint-disable-next-line
@@ -38,7 +36,7 @@ ArtZip.getInitialProps = async (appContext: AppContext) => {
   const allCookies = cookies(ctx);
   const accessTokenByCookie = allCookies['ACCESS_TOKEN'];
 
-  if (accessTokenByCookie !== undefined) {
+  if (!accessTokenByCookie) {
     const refreshTokenByCookie = allCookies['REFRESH_TOKEN'] || '';
     authRequest.interceptors.request.use(async (config) => {
       if (config.headers) {
