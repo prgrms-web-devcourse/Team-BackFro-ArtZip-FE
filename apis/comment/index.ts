a15fec92522a61255c3b294db233d55898d21ee0
Daeun Kim <o_o___t@pusan.ac.kr>
2022-08-14 01:40:04 +0900

    feat: 인증이 필요한 api 로직 변경

--- apis/comment/index.ts
+++ apis/comment/index.ts
@@ -1,17 +1,17 @@
 import { authRequest, unAuthRequest } from 'apis/common';
 import { CommentCreateRequest, CommentUpdateRequest } from 'types/apis/comment';
+import cookie from 'react-cookies';
 
 const commentAPI = {
   create: (reviewId: number, payload: CommentCreateRequest) => {
     return authRequest.post(`/api/v1/reviews/${reviewId}/comments`, payload);
   },
   update: (commentId: number, payload: CommentUpdateRequest) => {
-    authRequest.patch(`/api/v1/comments/${commentId}`, payload);
+    return authRequest.patch(`/api/v1/comments/${commentId}`, payload);
   },
   delete: (commentId: number) => {
-    authRequest.delete(`/api/v1/comments/${commentId}`);
+    return authRequest.delete(`/api/v1/comments/${commentId}`);
   },
-
   getReplies: ({
     commentId,
     pages,
@@ -21,12 +21,22 @@ const commentAPI = {
     pages?: number;
     size?: number;
   }) => {
-    return unAuthRequest.get(`/api/v1/comments/${commentId}/children`, {
-      params: {
-        ...(pages ? { pages: pages } : {}),
-        ...(size ? { size: size } : {}),
-      },
-    });
+    const refreshToken = cookie.load('REFRESH_TOKEN');
+    const params = {
+      ...(pages ? { pages: pages } : {}),
+      ...(size ? { size: size } : {}),
+    };
+
+    return refreshToken
+      ? authRequest.get(`/api/v1/comments/${commentId}/children`, {
+          params,
+        })
+      : unAuthRequest.get(`/api/v1/comments/${commentId}/children`, {
+          params,
+        });
+  },
+  likeToggle: (commentId: number) => {
+    return authRequest.patch(`/api/v1/comments/${commentId}/like`);
   },
 };
 
