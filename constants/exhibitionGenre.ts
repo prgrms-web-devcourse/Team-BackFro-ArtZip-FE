d4b3c8778fac8a5939d46a8941e999476b523b00
yunjjeongjo <two6885@gmail.com>
2022-08-14 14:47:15 +0900

    feat: exhibitionGenre constants 추가

---
+++ constants/exhibitionGenre.ts
@@ -0,0 +1,13 @@
+const exhibitionGenre = [
+  { id: 0, name: '기타', value: 'ETC' },
+  { id: 1, name: '그림', value: 'PAINTING' },
+  { id: 2, name: '사진', value: 'PHOTO' },
+  { id: 3, name: '조형', value: 'MOLDING' },
+  { id: 4, name: '설치', value: 'INSTALLATION' },
+  { id: 5, name: '미디어', value: 'MEDIA' },
+  { id: 6, name: '공연', value: 'SHOW' },
+  { id: 7, name: '역사', value: 'HISTORY' },
+  { id: 8, name: '공예', value: 'CRAFT' },
+  { id: 9, name: '만회애니', value: 'ANIMATION' },
+];
+export default exhibitionGenre;
