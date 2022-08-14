f4aa3a39378f3035cadfc14b0f65352093c77808
Hyevvy <hyeg0@naver.com>
2022-08-14 12:34:54 +0900

    feat: exhibitionGenre 추가

---
+++ constants/exhibitionGenre.ts
@@ -0,0 +1,14 @@
+const exhibitionGenre = [
+  { id: 0, name: '모든 장르', value: 'ALL' },
+  { id: 1, name: '그림', value: 'PAINTING' },
+  { id: 2, name: '사진', value: 'PHOTO' },
+  { id: 3, name: '조형', value: 'MOLDING' },
+  { id: 4, name: '설치', value: 'INSTALLATION' },
+  { id: 5, name: '미디어', value: 'MEDIA' },
+  { id: 6, name: '공연', value: 'SHOW' },
+  { id: 7, name: '역사', value: 'HISTORY' },
+  { id: 8, name: '공예', value: 'CRAFT' },
+  { id: 9, name: '만화애니', value: 'ANIMATION' },
+  { id: 10, name: '기타', value: 'ETC' },
+];
+export default exhibitionGenre;
--- constants/index.ts
+++ constants/index.ts
@@ -1,2 +1,3 @@
 export { default as exhibitionPeriod } from './exhibitionPeriod';
 export { default as exhibitionPlace } from './exhibitionPlace';
+export { default as exhibitionGenre } from './exhibitionGenre';
