03f4235818c17bcca75f6691d15b81557d8ef45a
Daeun Kim <o_o___t@pusan.ac.kr>
2022-08-14 19:02:02 +0900

    fix: 쿠키 옵션 삭제

--- utils/tokenManager/index.ts
+++ utils/tokenManager/index.ts
@@ -2,19 +2,13 @@ import cookie from 'react-cookies';
 
 function setToken(key: 'ACCESS_TOKEN' | 'REFRESH_TOKEN', token: string) {
   // 개발 환경에 따라서 설정
-  // const HTTP_ONLY = process.env.NEXT_PUBLIC_ENVIRONMENT === 'DEVELOP';
   const expires = new Date();
 
   expires.setDate(expires.getDate() + 14);
 
-  // TODO: 테스트용 주석, 배포 확정시 삭제
-  console.log('환경:', process.env.NEXT_PUBLIC_ENVIRONMENT);
-
   cookie.save(key, token, {
     path: '/',
     expires: key === 'REFRESH_TOKEN' ? expires : undefined,
-    domain: process.env.NEXT_PUBLIC_ENVIRONMENT === 'DEVELOP' ? '.localhost' : '.vercel.app',
-    sameSite: 'none',
     // httpOnly: HTTP_ONLY,
   });
 }
