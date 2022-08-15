fa043d19f951ce9c768fc08d1c56faf1271e4d77
Daeun Kim <74234333+dar-jeeling@users.noreply.github.com>
2022-08-15 16:19:17 +0900

    fix: _app 에서 접근하지 못하는 코드 삭제 (#204)
    * fix: _app 에서 접근하지 못하는 코드 삭제
    
    * test: 테스트 콘솔 제거
    
    * feat: setToken 의 쿠키에 httpOnly 옵션 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -47,18 +47,11 @@ ArtZip.getInitialProps = async (appContext: AppContext) => {
   const refreshTokenByCookie = allCookies['REFRESH_TOKEN'];
 
   // TODO: setToken의 로직 수정, 토큰 자체를 디코드하여 유효기간을 설정하기
+  // 현재 배포에서는 쿠키 확인 불가
   if (refreshTokenByCookie) {
     accessTokenByCookie && setToken('ACCESS_TOKEN', accessTokenByCookie);
     refreshTokenByCookie && setToken('REFRESH_TOKEN', refreshTokenByCookie);
-  } else {
-    cookie.remove('ACCESS_TOKEN');
-    cookie.remove('REFRESH_TOKEN');
   }
-
-  // else {
-  //   cookie.remove('ACCESS_TOKEN');
-  //   cookie.remove('REFRESH_TOKEN');
-  // }
 
   return { ...appProps };
 };
