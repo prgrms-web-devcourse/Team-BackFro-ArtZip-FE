7c9b5a9fe975b955f10aa860f1a0a69963b5cb5d
Daeun Kim <o_o___t@pusan.ac.kr>
2022-08-15 16:15:45 +0900

    feat: setToken 의 쿠키에 httpOnly 옵션 추가

--- utils/tokenManager/index.ts
+++ utils/tokenManager/index.ts
@@ -2,7 +2,7 @@ import cookie from 'react-cookies';
 
 function setToken(key: 'ACCESS_TOKEN' | 'REFRESH_TOKEN', token: string) {
   // 개발 환경에 따라서 설정
-  // const HTTP_ONLY = !(process.env.NEXT_PUBLIC_ENVIRONMENT === 'DEVELOP');
+  const HTTP_ONLY = !(process.env.NEXT_PUBLIC_ENVIRONMENT === 'DEVELOP');
   const expires = new Date();
 
   expires.setDate(expires.getDate() + 14);
@@ -11,7 +11,7 @@ function setToken(key: 'ACCESS_TOKEN' | 'REFRESH_TOKEN', token: string) {
     path: '/',
     expires: key === 'REFRESH_TOKEN' ? expires : undefined,
     // domain: process.env.NEXT_PUBLIC_ENVIRONMENT === 'DEVELOP' ? '' : '.artzip.shop',
-    // httpOnly: HTTP_ONLY,
+    httpOnly: HTTP_ONLY,
   });
 }
 
