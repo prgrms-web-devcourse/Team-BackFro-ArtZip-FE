759c85a81b2cdc14fca4dfce759b6e2eb3b5aaf4
Daeun Kim <o_o___t@pusan.ac.kr>
2022-08-21 01:34:28 +0900

    feat: 프록시 설정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: `https://artzip.shop/:path*`,
+      },
+    ];
+  },
   outputFileTracing: false,
   swcMinify: true,
   webpack(config) {
