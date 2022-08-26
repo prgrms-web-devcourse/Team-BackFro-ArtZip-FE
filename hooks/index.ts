7ed2e126b0836bd89ff4fb30ac97590fd1d3e62a
권기홍 <gitul0515@gmail.com>
2022-08-26 17:05:21 +0900

    feat: 비로그인 유저의 Private Page 접근 제한 (#230)
    * feat: Spinner 컴포넌트 추가
    
    * feat: 고차 컴포넌트 withAuth 추가
    
    * feat: 후기 생성/수정 페이지에서 widthAuth 적용
    
    * refactor: 고차컴포넌트 withAuth를 커스텀 훅 useWithAuth로 변경
    
    * design: Spinner 컴포넌트의 높이 확대
    
    * remove: 고차컴포넌트 withAuth 삭제

--- hooks/index.ts
+++ hooks/index.ts
@@ -3,3 +3,4 @@ export { default as useClickAway } from './useClickAway';
 export { default as useAxios } from './useAxios';
 export { default as useUserAuthActions } from './useUserAuthActions';
 export { default as useInput } from './useInput';
+export { default as useWithAuth } from './useWithAuth';
