125546df0e740239f15483688fbef205a6f9da64
Daeun Kim <o_o___t@pusan.ac.kr>
2022-08-30 18:34:30 +0900

    refactor: swrOption  분리

--- utils/index.ts
+++ utils/index.ts
@@ -1,6 +1,8 @@
+import { swrOptions } from './swrOptions/index';
 export { displayDate, displayDday, displayFormattedDate } from './displayedAt';
 export { default as storage } from './storage';
 export { default as throttleOnRendering } from './throttleOnRendering';
 export { objectToFormData, filesToFormData } from './formData';
 export { setToken } from './tokenManager';
 export { parseJwt } from './parseJwt';
+export { swrOptions } from './swrOptions';
