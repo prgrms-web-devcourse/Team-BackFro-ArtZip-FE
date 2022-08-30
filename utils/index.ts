8d4f26febc213bb4c8c6a9edeb1e44bbb875b295
gitul0515 <gitul0515@gmail.com>
2022-08-30 17:12:40 +0900

    feat: getErrorMessage 함수 추가

--- utils/index.ts
+++ utils/index.ts
@@ -4,3 +4,4 @@ export { default as throttleOnRendering } from './throttleOnRendering';
 export { objectToFormData, filesToFormData } from './formData';
 export { setToken } from './tokenManager';
 export { parseJwt } from './parseJwt';
+export { getErrorMessage } from './errorHandling';
