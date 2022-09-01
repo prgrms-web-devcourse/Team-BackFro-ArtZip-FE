d995501f44b80b0176b7788b2efc5b755046d3bb
gitul0515 <gitul0515@gmail.com>
2022-09-01 20:50:26 +0900

    refactor: validateReviewEditForm을 유틸 함수로 분리

--- utils/validation/index.ts
+++ utils/validation/index.ts
@@ -1,4 +1,6 @@
 import { RuleObject } from 'antd/lib/form';
+import { message } from 'antd';
+import { SubmitData } from 'pages/reviews/create/index';
 
 export const validateNickname = (_: RuleObject, value: string) => {
   if (!value) {
@@ -22,4 +24,36 @@ export const validatePassword = (_: RuleObject, value: string) => {
     return Promise.reject(new Error('8~13자 영문, 숫자, 특수문자를 사용하세요.'));
   }
   return Promise.resolve();
+};
+
+export const validateReviewEditForm = (submitData: SubmitData) => {
+  let isValidateComplete = false;
+  const { exhibitionId, date, title, content } = submitData;
+
+  if (!exhibitionId) {
+    message.error('다녀온 전시회를 등록해주세요.');
+    return isValidateComplete;
+  }
+  if (!date) {
+    message.error('다녀온 날짜를 입력해주세요.');
+    return isValidateComplete;
+  }
+  if (new Date(date) > new Date()) {
+    message.error('다녀온 날짜는 오늘 이후일 수 없습니다.');
+    return isValidateComplete;
+  }
+  if (!title) {
+    message.error('후기 제목을 입력해주세요.');
+    return isValidateComplete;
+  }
+  if (!content) {
+    message.error('후기 내용을 입력해주세요.');
+    return isValidateComplete;
+  }
+  if (content.length > 1000) {
+    message.error('내용은 1000자 이하로 작성해주세요.');
+    return isValidateComplete;
+  }
+  isValidateComplete = true;
+  return isValidateComplete;
 };
