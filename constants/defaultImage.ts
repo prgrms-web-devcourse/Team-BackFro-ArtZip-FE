1cdabc967c122590026b54ccb8d04f1e85925fdf
gitul0515 <gitul0515@gmail.com>
2022-09-08 21:43:16 +0900

    feat: 유저 프로필 기본 이미지 추가

--- constants/defaultImage.ts
+++ constants/defaultImage.ts
@@ -1,8 +1,8 @@
-const imageUrl = {
-  USER_DEFAULT:
+const defaultImage = {
+  USER_PROFILE:
     'https://user-images.githubusercontent.com/80658269/185602729-74e474d1-250a-4403-a5eb-d1e3ef3da863.png',
-  EXHIBITION_DEFAULT:
+  EXHIBITION_THUMBNAIL:
     'https://raw.githubusercontent.com/gitul0515/gitul0515.github.io/main/_posts/image/poster-default-image.png',
 };
 
-export default imageUrl;
+export default defaultImage;
