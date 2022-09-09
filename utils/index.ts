1ca59f47d79364c2aa644b0aece991e4b23f33f2
Daeun Kim <74234333+dar-jeeling@users.noreply.github.com>
2022-09-09 18:08:58 +0900

    feat: 커뮤니티 페이지 CSR 로직 SWR 으로 변경 (#248)
    * refactor: swrOption  분리
    
    * feat: 무한 스크롤 로직 SWR로 변경
    
    * refactor: 주석과 필요없는 import 삭제
    
    * refactor: 코드리뷰 반영

--- utils/index.ts
+++ utils/index.ts
@@ -4,5 +4,6 @@ export { default as throttleOnRendering } from './throttleOnRendering';
 export { convertObjectToFormData, convertFilesToFormData, getBase64 } from './converter';
 export { setToken } from './tokenManager';
 export { parseJwt } from './parseJwt';
+export { swrOptions } from './swrOptions';
 export { getErrorMessage } from './errorHandling';
 export { validateNickname, validatePassword, validateReviewEditForm } from './validation';
