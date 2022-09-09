9c5ebaaa442d9eb981f8b2231387a99c68cf132e
Hyevvy <hyeg0@naver.com>
2022-09-09 19:23:55 +0900

    feat: 변수명 대문자로 변경

--- constants/index.ts
+++ constants/index.ts
@@ -1,4 +1,4 @@
 export { SIGNOUT_USER_STATE } from './userLoginState';
-export { default as exhibitionPeriod } from './exhibitionPeriod';
-export { default as exhibitionPlace } from './exhibitionPlace';
-export { default as exhibitionGenre } from './exhibitionGenre';
+export { default as EXHIBITION_PERIOD } from './exhibitionPeriod';
+export { default as EXHIBITION_PLACE } from './exhibitionPlace';
+export { default as EXHIBITION_GENRE } from './exhibitionGenre';
