4db7e47a53db94a6b4a3fd730e94e644b30c8a23
Hyevvy <hyeg0@naver.com>
2022-09-09 20:11:44 +0900

    feat: 오타 수정

--- hooks/useWindowSize/index.ts
+++ hooks/useWindowSize/index.ts
@@ -4,14 +4,12 @@ const isSSR = typeof window === 'undefined';
 
 const useWindowSize = () => {
   const [windowWidthSize, setWindowWidthSize] = useState(0);
-  const [windowHeightSize, setWindowHieghtSize] = useState(0);
+  const [windowHeightSize, setWindowHeightSize] = useState(0);
   useEffect(() => {
-    // window resize를 위한 핸들러
     function handleResize() {
-      // 윈도우의 넓이/높이(width/height)를 set을 해준다
       if (!isSSR) {
         setWindowWidthSize(window.innerWidth);
-        setWindowHieghtSize(window.innerHeight);
+        setWindowHeightSize(window.innerHeight);
       }
     }
 
