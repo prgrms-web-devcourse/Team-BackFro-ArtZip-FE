c94ef96e8d44105bad4c30d7847bcce121800f90
gitul0515 <gitul0515@gmail.com>
2022-09-25 21:39:38 +0900

    refactor: SWR 전역 옵션 리팩토링
    - authRequest, unAuthRequest 재사용
    - 중복된 코드 통합

--- utils/swrOptions/index.ts
+++ utils/swrOptions/index.ts
@@ -1,24 +1,13 @@
-import axios from 'axios';
 import cookie from 'react-cookies';
+import { authRequest, unAuthRequest } from 'apis/common';
 
 export const swrOptions = {
   fetcher: async (url: string, params: object = {}) => {
     const isLoggedIn = cookie.load('REFRESH_TOKEN');
-    const accessToken = cookie.load('ACCESS_TOKEN');
-
-    if (isLoggedIn) {
-      const { data } = await axios.get(`${process.env.NEXT_PUBLIC_API_END_POINT}${url}`, {
-        headers: {
-          accessToken,
-        },
-        params,
-      });
-      return data.data;
-    } else {
-      const { data } = await axios.get(`${process.env.NEXT_PUBLIC_API_END_POINT}${url}`, {
-        params,
-      });
-      return data.data;
-    }
+    const request = isLoggedIn ? authRequest : unAuthRequest;
+    const { data } = await request.get(url, {
+      params,
+    });
+    return data.data;
   },
 };
