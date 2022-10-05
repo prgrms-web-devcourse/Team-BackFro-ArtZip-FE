f0bf1ccb7b60f647776e3907bdda91896f3527ff
gitul0515 <gitul0515@gmail.com>
2022-10-05 21:47:32 +0900

    refactor: 이미지 삭제 시, deletedPhotos의 불변성을 보장하도록 수정

--- components/organisms/ReviewEditForm/index.tsx
+++ components/organisms/ReviewEditForm/index.tsx
@@ -124,7 +124,7 @@ const ReviewEditForm = ({ type, prevData, onMutation }: ReviewEditFormProps) => 
   const handleImageDelete = () => {
     const photoId = clickedImage.current;
     const { deletedPhotos } = submitData.current;
-    deletedPhotos && (deletedPhotos as number[]).push(photoId);
+    handleChange('deletedPhotos', [...(deletedPhotos as number[]), photoId]);
     setPrevImages(prevImages.filter((image) => image.photoId !== photoId));
     setIsModalOn(false);
   };
