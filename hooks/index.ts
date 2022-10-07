b486aaa1af1f27201842b8143ca3fef0a1676edf
gitul0515 <gitul0515@gmail.com>
2022-10-07 17:47:29 +0900

    feat: index를 통해서 훅 내보내기

--- hooks/index.ts
+++ hooks/index.ts
@@ -2,6 +2,7 @@ export { default as useInfiniteScroll } from './useInfiniteScroll';
 export { default as useClickAway } from './useClickAway';
 export { default as useAxios } from './useAxios';
 export { default as useUserAuthActions } from './useUserAuthActions';
-export { default as useInput } from './useInput';
 export { default as useCheckAuth } from './useCheckAuth';
 export { default as useDebounce } from './useDebounce';
+export { default as useSessionStorage } from './useSessionStorage';
+export { default as useDraftReview } from './useDraftReview';
