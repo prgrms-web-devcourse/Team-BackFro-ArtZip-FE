9239b24074c6e14d0923f275c1fb1370d27c741b
gitul0515 <gitul0515@gmail.com>
2022-10-09 15:06:40 +0900

    refactor: TitleInput으로 유효성 검사 로직 이동

--- components/organisms/ReviewEditForm/fields/TitleInput/index.tsx
+++ components/organisms/ReviewEditForm/fields/TitleInput/index.tsx
@@ -2,30 +2,64 @@ import { Input } from 'antd';
 import { useEffect, useState } from 'react';
 import { ValueOf } from 'types/utility';
 import { SubmitData } from '../..';
+import ErrorMessage from '../../utils/ErrorMessage';
+
+const MAX_LENGTH = 30;
+const MESSAGE = {
+  NO_ERROR: '',
+  REQUIRED_VALUE: '필수 입력값입니다.',
+  EXCEEDED_MAX_LENGTH: `${MAX_LENGTH}자 이하로 작성해 주세요.`,
+};
 
 interface TitleInputProps {
   prevTitle?: string;
-  handleChange: (key: string, value: ValueOf<SubmitData>) => void;
+  wasSubmitted: boolean;
+  onChange: (key: string, value: ValueOf<SubmitData>) => void;
 }
 
-const TitleInput = ({ prevTitle, handleChange }: TitleInputProps) => {
+const TitleInput = ({ prevTitle, wasSubmitted, onChange }: TitleInputProps) => {
   const [title, setTitle] = useState(prevTitle || '');
+  const [touched, setTouched] = useState(false);
+  const [errorMessage, setErrormessage] = useState(
+    prevTitle ? MESSAGE.NO_ERROR : MESSAGE.REQUIRED_VALUE,
+  );
+  const displayErrorMessage = (wasSubmitted || touched) && !!errorMessage;
 
   useEffect(() => {
-    prevTitle && setTitle(prevTitle);
+    setTitle(prevTitle ? prevTitle : '');
+    setErrormessage(prevTitle ? MESSAGE.NO_ERROR : MESSAGE.REQUIRED_VALUE);
   }, [prevTitle]);
 
+  const handleChange = (value: string) => {
+    setTitle(value);
+    onChange('title', value);
+    validate(value);
+  };
+
+  const validate = (value: string) => {
+    if (!value) {
+      setErrormessage(MESSAGE.REQUIRED_VALUE);
+      return;
+    }
+    if (value.length > MAX_LENGTH) {
+      setErrormessage(MESSAGE.EXCEEDED_MAX_LENGTH);
+      return;
+    }
+    setErrormessage(MESSAGE.NO_ERROR);
+  };
+
   return (
-    <Input
-      placeholder="제목을 입력해주세요"
-      showCount
-      maxLength={30}
-      value={title}
-      onChange={(e) => {
-        handleChange('title', e.target.value);
-        setTitle(e.target.value);
-      }}
-    />
+    <>
+      <Input
+        placeholder="제목을 입력해주세요"
+        showCount
+        maxLength={30}
+        value={title}
+        onChange={(e) => handleChange(e.target.value)}
+        onBlur={() => setTouched(true)}
+      />
+      <ErrorMessage message={errorMessage} visible={displayErrorMessage} />
+    </>
   );
 };
 
