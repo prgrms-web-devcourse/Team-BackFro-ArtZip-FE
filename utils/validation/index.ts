14e5e4cc6edb43c8c8c4cbd5c5ebded700a7e321
권기홍 <gitul0515@gmail.com>
2022-10-10 20:30:26 +0900

    feat: 이미지 파일 유효성 검사 추가 (#321)
    * feat: 이미지 파일 유효성 검사를 유틸 함수로 추가
    
    * feat: ImageUpload 컴포넌트에 유효성 검사 적용
    
    * feat: 프로필 수정 페이지에 유효성 검사 적용

--- utils/validation/index.ts
+++ utils/validation/index.ts
@@ -1,5 +1,6 @@
 import { RuleObject } from 'antd/lib/form';
-import { message } from 'antd';
+import { RcFile } from 'antd/es/upload';
+import { message, Upload } from 'antd';
 import { SubmitData } from 'components/organisms/ReviewEditForm';
 
 export const validateNickname = (_: RuleObject, value: string) => {
@@ -56,4 +57,22 @@ export const validateReviewEditForm = (submitData: SubmitData) => {
   }
   isValidateComplete = true;
   return isValidateComplete;
+};
+
+export const validateImageFile = ({ type, size }: RcFile | File) => {
+  const ALLOWED_TYPES = ['image/png', 'image/jpg', 'image/jpeg'];
+  const MAX_SIZE_MB = 5;
+
+  const isAllowedType = ALLOWED_TYPES.includes(type);
+  if (!isAllowedType) {
+    message.error('png, jpg, jpeg 확장자 파일만 업로드할 수 있습니다.');
+    return isAllowedType || Upload.LIST_IGNORE;
+  }
+
+  const isAllowedSize = MAX_SIZE_MB >= size / 1024 / 1024;
+  if (!isAllowedSize) {
+    message.error(`${MAX_SIZE_MB}MB 이하의 파일만 업로드할 수 있습니다.`);
+    return isAllowedSize || Upload.LIST_IGNORE;
+  }
+  return true;
 };
