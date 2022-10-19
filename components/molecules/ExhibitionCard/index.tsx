f81f129ecaf59cc6a66ca5313eed44a6316995df
Daeun Kim <o_o___t@pusan.ac.kr>
2022-10-19 22:50:10 +0900

    design: 호버 시 text 가 제일 위로 올라오도록 수정

--- components/molecules/ExhibitionCard/index.tsx
+++ components/molecules/ExhibitionCard/index.tsx
@@ -21,54 +21,50 @@ const ExhibitionCard = ({ data }: ExhibitionCardProps) => {
   const { exhibitionId, name, thumbnail, startDate, endDate, likeCount, reviewCount, isLiked } =
     data;
 
-  // console.log('data', data);
-
   const dDay = displayDday(startDate);
   const mouseHover = () => setIsHover((isHover) => !isHover);
 
   return (
-    <Link href={`/exhibitions/detail/${exhibitionId}`}>
-      <S.ExhibitionCard>
-        <Card
-          className="exhibition-card"
-          onMouseEnter={mouseHover}
-          onMouseLeave={mouseHover}
-          cover={
-            <Image
-              src={thumbnail}
-              alt="card image"
-              layout="responsive"
-              sizes="(max-width: 767px) 200px, 250px"
-              width={250}
-              height={300}
-              placeholder="blur"
-              blurDataURL={DEFAULT_IMAGE.BLUR_DATA_URL}
-              className="card-image"
-            />
-          }
-        >
-          {isHover && (
-            <S.HoverContent>
-              {' '}
-              {isLiked ? <HeartFilled className="heart-icon" /> : <HeartOutlined />}
-              {likeCount} <MessageOutlined /> {reviewCount}{' '}
-            </S.HoverContent>
-          )}
-        </Card>
-        <S.Description>
-          <h3 className="title">{name}</h3>
-          <div>
-            <h3>
-              {displayFormattedDate(startDate)} - {displayFormattedDate(endDate)}
-            </h3>
-            <S.Dday>
-              D{dDay < 0 ? '+' : '-'}
-              {Math.abs(dDay)}
-            </S.Dday>
-          </div>
-        </S.Description>
-      </S.ExhibitionCard>
-    </Link>
+    <S.ExhibitionCardWrapper onMouseEnter={mouseHover} onMouseLeave={mouseHover}>
+      {isHover && (
+        <S.HoverContent>
+          {isLiked ? <HeartFilled className="heart-icon" /> : <HeartOutlined />}
+          {likeCount} <MessageOutlined /> {reviewCount}
+        </S.HoverContent>
+      )}
+      <Link href={`/exhibitions/detail/${exhibitionId}`}>
+        <S.ExhibitionCard>
+          <Card
+            className="exhibition-card"
+            cover={
+              <Image
+                src={thumbnail}
+                alt="card image"
+                layout="responsive"
+                sizes="(max-width: 767px) 200px, 250px"
+                width={250}
+                height={300}
+                placeholder="blur"
+                blurDataURL={DEFAULT_IMAGE.BLUR_DATA_URL}
+                className="card-image"
+              />
+            }
+          ></Card>
+          <S.Description>
+            <h3 className="title">{name}</h3>
+            <div>
+              <h3>
+                {displayFormattedDate(startDate)} - {displayFormattedDate(endDate)}
+              </h3>
+              <S.Dday>
+                D{dDay < 0 ? '+' : '-'}
+                {Math.abs(dDay)}
+              </S.Dday>
+            </div>
+          </S.Description>
+        </S.ExhibitionCard>
+      </Link>
+    </S.ExhibitionCardWrapper>
   );
 };
 
--- components/molecules/ExhibitionCard/style.ts
+++ components/molecules/ExhibitionCard/style.ts
@@ -1,5 +1,11 @@
 import styled from '@emotion/styled';
 
+export const ExhibitionCardWrapper = styled.div`
+  position: relative;
+  width: fit-content;
+  height: fit-content;
+`;
+
 export const ExhibitionCard = styled.div`
   background-color: ${({ theme }) => theme.color.background};
   box-shadow: 0px 6px 8px rgba(0, 0, 0, 0.25);
@@ -27,7 +33,7 @@ export const ExhibitionCard = styled.div`
   .ant-card-head {
     height: 0px;
   }
-  
+
   @media (max-width: ${({ theme }) => theme.breakPoint.mobile}) {
     width: 200px;
     height: 310px;
@@ -37,9 +43,7 @@ export const ExhibitionCard = styled.div`
 export const HoverContent = styled.div`
   z-index: 5;
   display: flex;
-  flex-direction: row;
   align-items: center;
-  justify-content: center;
   position: absolute;
   gap: 10px;
   color: white;
@@ -78,6 +82,7 @@ export const Description = styled.div`
 `;
 
 export const Title = styled.div``;
+
 export const Dday = styled.span`
   display: flex;
   align-items: center;
